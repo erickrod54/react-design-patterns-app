246bdf0ba2ac649ba2979c309acc7ccc3be6a85b
Erick Rodriguez <erickrod54@gmail.com>
2023-08-31 03:04:47 -0400

    react-design-patterns-app - version 1.00 - App js - Features: Setting the title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+/**react-design-patterns-app - version 1.00 - App js  
+ * - Features: 
+ * 
+ *     --> Setting the title
+ * 
+ * Note: This component will have later the main menu
+ * to each pattern and its explanations and use cases
+ */
 
 function App() {
   return (
-    <h2> React Design Patterns App </h2>
+    <h2 className="color-font"> React Design Patterns App </h2>
   );
 }
 
