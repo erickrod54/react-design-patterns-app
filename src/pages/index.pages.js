0a96c9b6d5ff68939ae3c1705e107a8332a66ada
Erick Rodriguez <erickrod54@gmail.com>
2023-09-04 01:16:35 -0400

    react-design-patterns-app - version 1.01 - index pages - Feature: Importing and exporting HomePage

---
+++ src/pages/index.pages.js
@@ -0,0 +1,12 @@
+import HomePage from "./home.page";
+
+/**react-design-patterns-app - version 1.01 - index pages 
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'HomePage'
+ * 
+ * Note: the cards components with the card information
+ * will be displayed here
+ */
+
+export { HomePage }
