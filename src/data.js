523807c1ee7d7e9b4ae77a06168548c540df2fcc
Erick Rodriguez <erickrod54@gmail.com>
2023-09-04 01:11:59 -0400

    react-design-patterns-app - version 1.01 - data js - Features: Building the patternCard data

---
+++ src/data.js
@@ -0,0 +1,41 @@
+/**react-design-patterns-app - version 1.01 - data js  
+ * - Features: 
+ * 
+ *     --> Building the 'patternCard' data
+ * 
+ * Note: This component will have later the main menu
+ * to each pattern and its explanations and use cases
+ */
+
+export const patternCard = [
+    {
+      id:1,
+      pattern:'Layout Components',
+      description:''
+    },
+    {
+      id:2,
+      pattern:'Container Components',
+      description:''
+    },
+    {
+      id:3,
+      pattern:'Controlled and Uncontrolled Components',
+      description:''
+    },
+    {
+      id:4,
+      pattern:'Higher-Order Components',
+      description:''
+    },
+    {
+      id:5,
+      pattern:'Custom Hooks Patterns',
+      description:''
+    },
+    {
+      id:6,
+      pattern:'Functional Programming and React',
+      description:''
+    },
+]
