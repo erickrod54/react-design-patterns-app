282f8359aeb0c5880b6a64bb8d169d5a481edfbf
Erick Rodriguez <erickrod54@gmail.com>
2023-09-04 01:14:12 -0400

    react-design-patterns-app - version 1.01 - index js  - Features: Wrapping the App with the provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { PatternsAppProvider } from './context';
+
+/**react-design-patterns-app - version 1.01 - index js  
+ * - Features: 
+ * 
+ *     --> Wrapping the 'App' with the provider
+ * 
+ * Note: This component will have later the main menu
+ * to each pattern and its explanations and use cases
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PatternsAppProvider>
+      <App />
+    </PatternsAppProvider>
   </React.StrictMode>
 );
 
