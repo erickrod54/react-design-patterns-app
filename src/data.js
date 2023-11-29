07cfa58a129b7771b6db2e8afea5a4bda3c4c9c4
Erick Rodriguez <erickrod54@gmail.com>
2023-11-28 21:30:00 -0500

    react-design-patterns-app - version 1.02 - data js - Features: Adding more patterns to patternCard

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
-/**react-design-patterns-app - version 1.01 - data js  
+/**react-design-patterns-app - version 1.02 - data js  
  * - Features: 
  * 
- *     --> Building the 'patternCard' data
+ *     --> Adding more patterns to 'patternCard'
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -36,6 +36,126 @@ export const patternCard = [
     {
       id:6,
       pattern:'Functional Programming and React',
+      description:''
+    },
+    {
+      id:7,
+      pattern:'Advanced Concepts and Hooks',
+      description:''
+    },
+    {
+      id:8,
+      pattern:'Design System Core Concepts',
+      description:''
+    },
+    {
+      id:9,
+      pattern:'Building Components Using Figma',
+      description:''
+    },
+    {
+      id:10,
+      pattern:'Design System - React Composition',
+      description:''
+    },
+    {
+      id:11,
+      pattern:'Design System - Developing Components',
+      description:''
+    },
+    {
+      id:12,
+      pattern:'Design System - Encapsulating Styles',
+      description:''
+    },
+    {
+      id:13,
+      pattern:'Design System - Patterns for Spacing',
+      description:''
+    },
+    {
+      id:14,
+      pattern:'Design System - Patterns for more Complex Styles',
+      description:''
+    },
+    {
+      id:15,
+      pattern:'Performance - Optimization',
+      description:''
+    },
+    {
+      id:16,
+      pattern:'Performance - Fixing Rendering issues',
+      description:''
+    },
+    {
+      id:17,
+      pattern:'Performance - Expensive operations',
+      description:''
+    },
+    {
+      id:18,
+      pattern:'Clean Code Tips',
+      description:''
+    },
+    {
+      id:19,
+      pattern:'Advanced TypeScript: Introduction',
+      description:''
+    },
+    {
+      id:20,
+      pattern:'Advanced TypeScript: Basics',
+      description:''
+    },
+    {
+      id:21,
+      pattern:'Advanced TypeScript: Typing Hooks',
+      description:''
+    },
+    {
+      id:22,
+      pattern:'Advanced TypeScript: Typing reducers',
+      description:''
+    },
+    {
+      id:23,
+      pattern:'Advanced TypeScript: Typing Context',
+      description:''
+    },
+    {
+      id:24,
+      pattern:'Advanced TypeScript: Using Generics',
+      description:''
+    },
+    {
+      id:25,
+      pattern:'Advanced TypeScript: More on TypeScript',
+      description:''
+    },
+    {
+      id:26,
+      pattern:'Advanced TypeScript: Component Patterns',
+      description:''
+    },
+    {
+      id:27,
+      pattern:'React Router: Introduction',
+      description:''
+    },
+    {
+      id:28,
+      pattern:'React Router: Basic Concepts',
+      description:''
+    },
+    {
+      id:29,
+      pattern:'React Router: Navigations and links',
+      description:''
+    },
+    {
+      id:30,
+      pattern:'React Router: Advanced Routing',
       description:''
     },
 ]
