6c1f0e2b94e08b11a9f2c00e4e8be97732f13df1
Erick Rodriguez <erickrod54@gmail.com>
2023-11-28 21:30:00 -0500

    react-design-app - version 1.02 - tailwind.config.js - Features: Adding tailwind content routes

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/**react-design-patterns-app - version 1.02 - tailwind.config.js 
+ * - Features: 
+ * 
+ *     --> Adding tailwind content routes.
+ * 
+ * Note: this is made in order to apply styles
+ */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
