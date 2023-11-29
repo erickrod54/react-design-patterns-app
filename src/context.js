360c1f58a176c2ba22fe00483570fb94f00e4f40
Erick Rodriguez <erickrod54@gmail.com>
2023-11-28 21:30:00 -0500

    react-design-patterns-app - version 1.02 - context js - Features: Placing and Provising pattenCard

--- src/context.js
+++ src/context.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react"
+import { patternCard } from "./data";
 
-/**react-design-patterns-app - version 1.01 - context js  
+/**react-design-patterns-app - version 1.02 - context js  
  * - Features: 
  * 
- *     --> Building the context, providers and context
- *         hook.
+ *     --> Placing and providing 'patternCard'.
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -15,11 +15,13 @@ const PatternsAppContext = React.createContext();
 
 export const PatternsAppProvider = ({ children }) => {
 
-    const test = 'testing pattern context'
+    //const test = 'testing pattern context'
 
     return(
         <PatternsAppContext.Provider
-             value={{test}}
+             value={{
+                patternCard
+             }}
         >
             {children}
         </PatternsAppContext.Provider>
