8631bf8e9ade2fa0476b3a7f8f9b83797701d494
Erick Rodriguez <erickrod54@gmail.com>
2023-11-29 21:30:00 -0500

    react-design-patterns-app - version 1.05 - HomePage - Features: Adding breakpoints for max-sm and max-2xl

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { usePatternsAppContext } from "../context";
 
-/**react-design-patterns-app - version 1.04 - HomePage 
+/**react-design-patterns-app - version 1.05 - HomePage 
  * - Features: 
  * 
- *     --> Responsive design - work in progress
+ *     --> Adding breakpoints for 'max-sm' and 'max-2xl'
  * 
  * Note: the cards components with the card information
  * will be displayed here
@@ -17,24 +17,24 @@ const HomePage = () =>  {
     console.log({patternCard})
 
     return(
-        <>
-            
-            <div className="m-8 p-8 sm:grid grid-cols-8 gap-6 max-sm:grid-cols-2">
+            <div className="m-8 p-8 grid grid-cols-8 gap-2 max-2xl:grid-cols-4 max-sm:grid-cols-1 ">
                 {patternCard.map((Card) => {
 
                     const { id, pattern } = Card
                     return(
-                        <div key={id} className="bg-gray-400 p-4 flex
-                                 justify-center items-center
-                                 shadow-md border
-                                 border-green-500 border-6 cursor-pointer hover:scale-110 hover:rounded-lg duration-75">{pattern}</div>
+                        <ul>
+                            <li className="m-2">
+                            <div key={id} className="bg-gray-400 p-4 flex
+                                     m-4
+                                     justify-center items-center
+                                     shadow-md border
+                                     border-green-500 border-6 cursor-pointer hover:scale-110 hover:rounded-lg duration-75">{pattern}</div>
+                            </li>
+                        </ul>
                     )
                 })}
                 
             </div>
- 
-
-        </>
     )
 }
 
