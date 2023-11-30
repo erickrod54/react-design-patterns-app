17dac77de4b1002abcddc82bfb2681144feedf8c
Erick Rodriguez <erickrod54@gmail.com>
2023-11-29 21:30:00 -0500

    react-design-patterns-app - version 1.04 - HomePage - Features: Responsive design - work in progress

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { usePatternsAppContext } from "../context";
 
-/**react-design-patterns-app - version 1.03 - HomePage 
+/**react-design-patterns-app - version 1.04 - HomePage 
  * - Features: 
  * 
- *     --> Pending make responsive design
+ *     --> Responsive design - work in progress
  * 
  * Note: the cards components with the card information
  * will be displayed here
@@ -19,12 +19,15 @@ const HomePage = () =>  {
     return(
         <>
             
-            <div className="m-4 p-4 grid grid-cols-8 gap-6">
+            <div className="m-8 p-8 sm:grid grid-cols-8 gap-6 max-sm:grid-cols-2">
                 {patternCard.map((Card) => {
 
                     const { id, pattern } = Card
                     return(
-                        <div key={id} className="bg-gray-400 p-4 flex justify-center items-center shadow-md border border-green-500 border-6 cursor-pointer">{pattern}</div>
+                        <div key={id} className="bg-gray-400 p-4 flex
+                                 justify-center items-center
+                                 shadow-md border
+                                 border-green-500 border-6 cursor-pointer hover:scale-110 hover:rounded-lg duration-75">{pattern}</div>
                     )
                 })}
                 
