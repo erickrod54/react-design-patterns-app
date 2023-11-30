cdd997f331cc23f43070322075d4e0f987844270
Erick Rodriguez <erickrod54@gmail.com>
2023-11-29 21:30:00 -0500

    react-design-patterns-app - version 1.03 - HomePage - Features: Pending make responsive design

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { usePatternsAppContext } from "../context";
 
-/**react-design-patterns-app - version 1.01 - HomePage 
+/**react-design-patterns-app - version 1.03 - HomePage 
  * - Features: 
  * 
- *     --> Building 'HomePage' page
- * 
- *     --> testing the context.
+ *     --> Pending make responsive design
  * 
  * Note: the cards components with the card information
  * will be displayed here
@@ -15,10 +13,25 @@ import { usePatternsAppContext } from "../context";
 
 const HomePage = () =>  {
     
-    const { test } = usePatternsAppContext()
-    console.log('this is the test message ==>', test)
+    const { patternCard } = usePatternsAppContext()
+    console.log({patternCard})
+
     return(
-        <h2>Home Page</h2>
+        <>
+            
+            <div className="m-4 p-4 grid grid-cols-8 gap-6">
+                {patternCard.map((Card) => {
+
+                    const { id, pattern } = Card
+                    return(
+                        <div key={id} className="bg-gray-400 p-4 flex justify-center items-center shadow-md border border-green-500 border-6 cursor-pointer">{pattern}</div>
+                    )
+                })}
+                
+            </div>
+ 
+
+        </>
     )
 }
 
