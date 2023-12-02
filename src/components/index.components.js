818dd3906115d42142d0ff03d08feaaa2d886ae1
Erick Rodriguez <erickrod54@gmail.com>
2023-12-01 21:30:00 -0500

    reac-design-patterns-app - version 1.08 - components > index - Features: Adding LayoutComponentPattern route

---
+++ src/components/index.components.js
@@ -0,0 +1,11 @@
+import LayoutComponentPattern from "./laytout.pattern.component";
+
+/**react-design-patterns-app - version 1.08 - components > index  
+ * - Features: 
+ * 
+ *     --> Adding 'LayoutComponentPattern' route
+ * 
+ * Note: Pending to make responsive design
+ */
+
+export { LayoutComponentPattern }
