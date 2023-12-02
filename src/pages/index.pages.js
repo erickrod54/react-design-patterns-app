8572171e24b02d3080c8c6499ab524f186ced5dc
Erick Rodriguez <erickrod54@gmail.com>
2023-12-01 21:30:00 -0500

    react-design-patterns-app - version 1.08 - index > pages - Features: Importing and exporting SinglePattern

--- src/pages/index.pages.js
+++ src/pages/index.pages.js
@@ -1,12 +1,13 @@
 import HomePage from "./home.page";
+import SinglePatternPage from "./single.pattern.page";
 
-/**react-design-patterns-app - version 1.01 - index pages 
+/**react-design-patterns-app - version 1.08 - index pages 
  * - Features: 
  * 
- *     --> Importing and exporting 'HomePage'
+ *     --> Importing and exporting 'SinglePattern'
  * 
  * Note: the cards components with the card information
  * will be displayed here
  */
 
-export { HomePage }
+export { HomePage, SinglePatternPage }
