502ebb35fca02937f3a21f4815c474248f47ad0b
Erick Rodriguez <erickrod54@gmail.com>
2023-12-02 21:30:00 -0500

    react-design-patterns-app - version 1.10 - SharedLayout - Features: Adding Link to back Home

--- src/components/shared.layout.component.jsx
+++ src/components/shared.layout.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-/**react-design-patterns-app - version 1.09 - SharedLayout 
+/**react-design-patterns-app - version 1.10 - SharedLayout 
  * - Features: 
  * 
- *     --> Builting 'SharedLayout'
+ *     --> Adding link to back Home
  * 
  * Note: This is the first topic
  */
@@ -11,9 +12,12 @@ import React from "react";
 const SharedLayout = ({ children }) => {
 
     return(
-        <div className="flex justify-center items-center">
-            {children}
-        </div>
+        <>
+            <Link to='/' className="bg-black-600 p-6 flex justify-center items-center shadow-md border border-green-500 border-6 rounded-xl w-fit m-16 hover:bg-blue-500">Back Home</Link>
+            <div className="flex justify-center items-center mt-10">
+                {children}
+            </div>
+        </>
     )
 }
 
