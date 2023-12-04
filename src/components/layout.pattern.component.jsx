2e2bcd466016bafe51372c382e200b88606712d3
Erick Rodriguez <erickrod54@gmail.com>
2023-12-03 21:30:00 -0500

    react-design-patterns-app - version 1.10 - LayoutComponentPattern - Features: Modifying file name

---
+++ src/components/layout.pattern.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**react-design-patterns-app - version 1.10 - LayoutComponentPattern 
+ * - Features: 
+ * 
+ *     --> Modifying file name
+ * 
+ * Note: This is the first topic
+ */
+
+const LayoutComponentPattern = () => {
+
+    return(
+        <h2>Layout Component Pattern</h2>
+    )
+}
+
+export default LayoutComponentPattern;
