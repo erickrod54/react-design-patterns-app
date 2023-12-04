bcb9fc4688a26d475f0a24fa9ea96f858189a73f
Erick Rodriguez <erickrod54@gmail.com>
2023-12-03 21:30:00 -0500

    react-design-patterns-app - version 1.10 - CustomTitle - Features: Building CustomTitle

---
+++ src/components/custom.title.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**react-design-patterns-app - version 1.10 - CustomTitle  
+ * - Features: 
+ * 
+ *     --> Building 'CustomTitle'
+ * 
+ * Note: Pending to make responsive design
+ */
+
+const CustomTitle = ({ title }) => {
+
+    return(
+        <p className="text-2xl">
+            {title}
+        </p>
+    )
+}
+
+export default CustomTitle
