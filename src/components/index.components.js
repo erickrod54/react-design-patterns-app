5e4a0042cf49bb9103facbc24873f06004e0bc90
Erick Rodriguez <erickrod54@gmail.com>
2023-12-03 21:30:00 -0500

    react-design-patterns-app - version 1.10 - components > index - Features: Importing and exporting  CustomTitle

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,12 +1,13 @@
-import LayoutComponentPattern from "./laytout.pattern.component";
+import CustomTitle from "./custom.title.component";
+import LayoutComponentPattern from "./layout.pattern.component";
 import SharedLayout from "./shared.layout.component";
 
-/**react-design-patterns-app - version 1.09 - components > index  
+/**react-design-patterns-app - version 1.10 - components > index  
  * - Features: 
  * 
- *     --> Adding 'SharedLayout' route
+ *     --> Importing and exporting 'CustomTitle'
  * 
  * Note: Pending to make responsive design
  */
 
-export { LayoutComponentPattern, SharedLayout}
+export { LayoutComponentPattern, SharedLayout, CustomTitle}
