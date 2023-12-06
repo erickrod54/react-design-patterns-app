051a1394e730df9cef51a9cb60775469c554e222
Erick Rodriguez <erickrod54@gmail.com>
2023-12-05 21:30:00 -0500

    react-design-patterns-app - version 1.13 - components > index - Features: Importing and exporting IntroComponent

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,13 +1,14 @@
 import CustomTitle from "./custom.title.component";
+import IntroComponent from "./intro.component";
 import LayoutComponentPattern from "./layout.pattern.component";
 import SharedLayout from "./shared.layout.component";
 
-/**react-design-patterns-app - version 1.10 - components > index  
+/**react-design-patterns-app - version 1.13 - components > index  
  * - Features: 
  * 
- *     --> Importing and exporting 'CustomTitle'
+ *     --> Importing and exporting 'IntroComponent'
  * 
  * Note: Pending to make responsive design
  */
 
-export { LayoutComponentPattern, SharedLayout, CustomTitle}
+export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent}
