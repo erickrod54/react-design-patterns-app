392f32e28c39907ddc9669728e77b742f67f8df6
Erick Rodriguez <erickrod54@gmail.com>
2023-12-05 21:30:00 -0500

    react-design-patterns-app - version 1.13 - CustomTitle - Features: Getting rid underline prop

--- src/components/custom.title.component.jsx
+++ src/components/custom.title.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-/**react-design-patterns-app - version 1.11 - CustomTitle  
+/**react-design-patterns-app - version 1.13 - CustomTitle  
  * - Features: 
  * 
- *     --> Adding 'custom' tailwind css
+ *     --> Get rid of 'underline' prop
  * 
  * Note: Pending to make responsive design
  */
@@ -11,7 +11,7 @@ import React from "react";
 const CustomTitle = ({ title }) => {
 
     return(
-        <p className="text-4xl underline capitalize">
+        <p className="text-4xl capitalize">
             {title}
         </p>
     )
