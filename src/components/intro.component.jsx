0c319ae798c91a66245a73aed5f633caaa03c581
Erick Rodriguez <erickrod54@gmail.com>
2023-12-06 21:30:00 -0500

    react-design-patterns-app - version 1.15 - IntroComponent - Features: Destructuring IntroComponentData

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import CustomTitle from "./custom.title.component";
+import { usePatternsAppContext } from "../context";
 
-/**react-design-patterns-app - version 1.14 - IntroComponent 
+/**react-design-patterns-app - version 1.15 - IntroComponent 
  * - Features: 
  * 
- *     --> Writting intro content
+ *     --> Destructuring IntroData
  * 
  * Note: This is the first topic
  */
 
 const IntroComponent = () => {
 
+    const { IntroComponentData } = usePatternsAppContext();
+
+    console.log({ IntroComponentData })
+
+    const { title, description } = IntroComponentData[0];
+
     return(
         <div className="flex justify-center items-center w-92">
             <div className="flex-col ">
                 <p className=" text-red-300 m-4 p-4">
-                    <CustomTitle title={'Introduction to React Pattterns'} />
+                    <CustomTitle title={title} />
                 </p>
 
                 <div className="m-4 p-4">
                     <p className="flex justify-center items-center">
-                        Design patterns are effective solutions for common challenges
-                        and some of this are specific patterns that come from actual 
-                        industry experience:
+                        {description}:
                     </p>
 
                     <ul className="list-decimal	mt-6 ml-12">
