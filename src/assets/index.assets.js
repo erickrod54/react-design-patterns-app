6518e9e8313cf7238f94ac64a128517e14c99e40
Erick Rodriguez <erickrod54@gmail.com>
2023-12-07 21:30:00 -0500

    react-design-patterns-app - version 1.17 - assets > index - Features: Importing and exporting layoutexamples asset

---
+++ src/assets/index.assets.js
@@ -0,0 +1,12 @@
+import layoutexamples from './layout-examples.png'
+
+/**react-design-patterns-app - version 1.17 - assets > index 
+ * - Features: 
+ * 
+ *     --> importing and exporting 'layoutexamples' 
+ * 
+ * Note: This component will have later the main menu
+ * to each pattern and its explanations and use cases
+ */
+
+export { layoutexamples }
