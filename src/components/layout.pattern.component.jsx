461ccf52827693f9cb98e2e0de897057083e6eef
Erick Rodriguez <erickrod54@gmail.com>
2023-12-07 21:30:00 -0500

    react-design-patterns-app - version 1.17 - LayoutComponentPattern - Features: Starting LayoutComponent

--- src/components/layout.pattern.component.jsx
+++ src/components/layout.pattern.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import CustomTitle from "./custom.title.component";
 
-/**react-design-patterns-app - version 1.13 - LayoutComponentPattern 
+/**react-design-patterns-app - version 1.17 - LayoutComponentPattern 
  * - Features: 
  * 
- *     --> Adding 'underline' prop
+ *     --> Starting 'LayoutComponentPattern'
  * 
  * Note: This is the first topic
  */
@@ -12,8 +12,36 @@ import CustomTitle from "./custom.title.component";
 const LayoutComponentPattern = () => {
 
     return(
+        <div>
         <div className="text-yellow-100 underline">
             <CustomTitle title={'layout component pattern'} />
+        </div>
+        
+        <section className="mt-12 first-letter:capitalize text-lg">
+            <p>
+                layout components focus on organizing other
+                components within a web page, some examples
+                of layout components can be:
+            </p>
+            
+            <ul className="m-4 p-4 list-decimal">
+                <li>
+                    Split Screen
+                </li>
+                <li>
+                    List
+                </li>
+                <li>
+                    Modals
+                </li>
+            </ul>
+            
+            <p>
+                as can be seen in the following picture:
+            </p>
+
+            
+        </section>
         </div>
     )
 }
