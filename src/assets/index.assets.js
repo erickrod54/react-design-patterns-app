065c1314a1abe09e0dfe35b468cbc247b220334c
Erick Rodriguez <erickrod54@gmail.com>
2023-12-08 21:30:00 -0500

    react-design-patterns-app - version 1.20 - assets > index - Features: Importing and exporting sidebarexample

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,12 +1,13 @@
 import layoutexamples from './layout-examples.png'
+import sidebarexample from './side-bar-example.png'
 
-/**react-design-patterns-app - version 1.17 - assets > index 
+/**react-design-patterns-app - version 1.20 - assets > index 
  * - Features: 
  * 
- *     --> importing and exporting 'layoutexamples' 
+ *     --> importing and exporting 'sidebarexample' 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
  */
 
-export { layoutexamples }
+export { layoutexamples, sidebarexample }
