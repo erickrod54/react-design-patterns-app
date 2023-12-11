b95b98d0e1fb95e5a27d5f2fc8cf4ddd84277ec5
Erick Rodriguez <erickrod54@gmail.com>
2023-12-10 21:30:00 -0500

    react-design-patterns-app - version 2.03 - styles.components - Features: Writting styles fro section wrapper

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 2.02 - styled.components
+/**react-design-patterns-app - version 2.03 - styled.components
  * - Features: 
  * 
- *     --> Building SectionWrapper 
+ *     --> Writting styles for section wrapper 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,4 +16,16 @@ import tw from 'twin.macro';
 
 export const SectionWrapper = styled.section`
     ${tw`m-12 p-12 mt-12 first-letter:capitalize text-lg`}
+
+    ul{
+        ${tw`m-4 p-4 list-decimal`} 
+    }
+
+    img{
+        ${tw`mt-10 w-auto h-auto`}
+    }
+
+    p{
+        ${tw`mt-6 mb-8 first-letter:capitalize`}
+    }
 `
