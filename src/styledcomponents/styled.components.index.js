2b0056cedf79e7cf96951d199919e678b3b35c7d
Erick Rodriguez <erickrod54@gmail.com>
2023-12-10 21:30:00 -0500

    react-design-patterns-app - version 2.02 - styledcomponents > index - Features: Importing and exporting SectionWrapper

---
+++ src/styledcomponents/styled.components.index.js
@@ -0,0 +1,16 @@
+import { SectionWrapper } from "./styled.components";
+
+/**react-design-patterns-app - version 2.02 - styledcomponents > index
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'SectionWrapper'  
+ * 
+ * Note: the macro 'tw' in order to work and use has to be installed
+ * there is around 4 or 5 libraries and this is oen of the most active
+ * and recent
+ * 
+ *          https://www.npmjs.com/package/twin.macro
+ */
+
+/**Sections and content */
+export { SectionWrapper }
