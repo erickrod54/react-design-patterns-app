f2d881b43caaeabc1b23510c3666ca09c85d408d
Erick Rodriguez <erickrod54@gmail.com>
2023-12-10 21:30:00 -0500

    react-design-patterns-app - version 2.03 - LayoutComponentPattern - Features: Migrating styles for SectionWrapper

--- src/components/layout.pattern.component.jsx
+++ src/components/layout.pattern.component.jsx
@@ -3,10 +3,10 @@ import CustomTitle from "./custom.title.component";
 import { usePatternsAppContext } from "../context";
 import { SectionWrapper } from "../styledcomponents/styled.components.index";
 
-/**react-design-patterns-app - version 2.02 - LayoutComponentPattern 
+/**react-design-patterns-app - version 2.03 - LayoutComponentPattern 
  * - Features: 
  * 
- *     --> Importing and placing 'SectionWrapper'
+ *     --> Migrating styles for 'SectionWrapper'
  * 
  * Note: This is the first topic
  */
@@ -34,7 +34,7 @@ const LayoutComponentPattern = () => {
                 of layout components can be:
             </p>
             
-            <ul className="m-4 p-4 list-decimal">
+            <ul>
                 <li>
                     Split Screen
                 </li>
@@ -50,24 +50,24 @@ const LayoutComponentPattern = () => {
                 as can be seen in the following picture:
             </p>
 
-            <img src={layoutexamples} alt="layoutexamples" className="mt-10 w-auto h-auto"/>
+            <img src={layoutexamples} alt="layoutexamples" />
 
-            <p className="mt-6">
+            <p>
                 an use case can be when is developed a side navigation
                 developers usually tend to include both as the html associated
                 styles within component itself as the image that follows:
             </p>
 
-            <img src={sidebarexample} alt="layoutexamples" className="mt-10 w-auto h-auto"/>
+            <img src={sidebarexample} alt="layoutexamples" />
 
-            <p className="mt-6">
+            <p>
                 however with layout components adopt different approaches
                 by separating the actual layout styles into a dedicated component
                 and only insert the specific components like the side navigation
                 into the layout component
             </p>
 
-            <p className="mt-6 mb-8 first-letter:capitalize">
+            <p>
                 the <span className="text-white font-semibold">main advantage of using layout components </span> 
                 is that the individual components, the core content of a web
                 page <span className="text-white font-semibold">should be unaware and unconcerned about their 
