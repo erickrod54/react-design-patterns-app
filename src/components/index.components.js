a4333e15842b86572835554f3c30ae9f51c7f087
Erick Rodriguez <erickrod54@gmail.com>
2023-12-11 21:30:00 -0500

    react-design-patterns-app - version 2.05 - components > index - Features: Importing and exporting SplitScreen

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -2,13 +2,14 @@ import CustomTitle from "./custom.title.component";
 import IntroComponent from "./intro.component";
 import LayoutComponentPattern from "./layout.pattern.component";
 import SharedLayout from "./shared.layout.component";
+import SplitScreen from "./split.screen.component";
 
-/**react-design-patterns-app - version 1.13 - components > index  
+/**react-design-patterns-app - version 2.05 - components > index  
  * - Features: 
  * 
- *     --> Importing and exporting 'IntroComponent'
+ *     --> Importing and exporting 'SplitScreen'
  * 
  * Note: Pending to make responsive design
  */
 
-export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent}
+export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent, SplitScreen}
