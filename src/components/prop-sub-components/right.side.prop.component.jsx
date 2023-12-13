0a0f55e276e414d608ca57c8751e94b4f957cb09
Erick Rodriguez <erickrod54@gmail.com>
2023-12-12 21:30:00 -0500

    react-design-patterns-app - version 2.05 - RightSideComponent - Features: Building Basic RightSideComponent

---
+++ src/components/prop-sub-components/right.side.prop.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**react-design-patterns-app - version 2.05 - RightSideComponent 
+ * - Features: 
+ * 
+ *     --> Building Basic 'RightSideComponent'
+ * 
+ * Note: This is the first topic
+ */
+
+const RightSideComponent = () => {
+    return(
+        <h2>I am Right</h2>
+    )
+}
+
+export default RightSideComponent;
