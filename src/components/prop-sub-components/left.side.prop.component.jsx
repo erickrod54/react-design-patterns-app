b19f6c7a6df572383afdd01f6c8417d5c5473b32
Erick Rodriguez <erickrod54@gmail.com>
2023-12-12 21:30:00 -0500

    react-design-patterns-app - version 2.05 - LeftSideComponent - Features: Building Basic LeftSideComponent

---
+++ src/components/prop-sub-components/left.side.prop.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**react-design-patterns-app - version 2.05 - LeftSideComponent 
+ * - Features: 
+ * 
+ *     --> Building Basic 'LeftSideComponent'
+ * 
+ * Note: This is the first topic
+ */
+
+
+const LeftSideComponent = () => {
+    return(
+        <h2>I am Left</h2>
+    )
+}
+
+export default LeftSideComponent;
