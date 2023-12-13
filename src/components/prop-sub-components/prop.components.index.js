e0a2bee774a655bb8d14997a3a32d67e934435bc
Erick Rodriguez <erickrod54@gmail.com>
2023-12-12 21:30:00 -0500

    react-design-patterns-app - version 2.05 - prop-sub-components > index - Features: Importing and exporting lefSideComponent and RightSideComponent

---
+++ src/components/prop-sub-components/prop.components.index.js
@@ -0,0 +1,12 @@
+import LeftSideComponent from "./left.side.prop.component";
+import RightSideComponent from "./right.side.prop.component";
+
+/**react-design-patterns-app - version 2.05 - prop-sub-components > index 
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'LeftSideComponent', and 'RightSideComponent' 
+ * 
+ * Note: This is the first topic
+ */
+
+export { LeftSideComponent, RightSideComponent }
