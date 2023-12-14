9758d2ef02215a966dc5aec365281f2582a8d157
Erick Rodriguez <erickrod54@gmail.com>
2023-12-14 12:24:44 -0500

    react-design-patterns-app - version 2.10 - assets > index - Features: Importing and exporting clipboard

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,13 +1,14 @@
 import layoutexamples from './layout-examples.png'
 import sidebarexample from './side-bar-example.png'
+import clipboard from './copy.png'
 
-/**react-design-patterns-app - version 1.20 - assets > index 
+/**react-design-patterns-app - version 2.10 - assets > index 
  * - Features: 
  * 
- *     --> importing and exporting 'sidebarexample' 
+ *     --> importing and exporting 'clipboard' 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
  */
 
-export { layoutexamples, sidebarexample }
+export { layoutexamples, sidebarexample, clipboard }
