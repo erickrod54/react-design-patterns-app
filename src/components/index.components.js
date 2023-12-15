e2413bd5867d449e246559a8214f499d321c701b
Erick Rodriguez <erickrod54@gmail.com>
2023-12-15 01:29:37 -0500

    react-design-patterns-app - version 2.11 - components > index - Features: Importing and exporting SplitScreenVariation

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -3,13 +3,14 @@ import IntroComponent from "./intro.component";
 import LayoutComponentPattern from "./layout.pattern.component";
 import SharedLayout from "./shared.layout.component";
 import SplitScreen from "./split.screen.component";
+import SplitScreenVariation from "./split.screen.variation.component";
 
-/**react-design-patterns-app - version 2.05 - components > index  
+/**react-design-patterns-app - version 2.11 - components > index  
  * - Features: 
  * 
- *     --> Importing and exporting 'SplitScreen'
+ *     --> Importing and exporting 'SplitScreenVariation'
  * 
  * Note: Pending to make responsive design
  */
 
-export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent, SplitScreen}
+export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent, SplitScreen, SplitScreenVariation}
