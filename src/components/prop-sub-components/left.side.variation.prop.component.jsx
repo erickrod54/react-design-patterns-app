35fe4f85345661e04242636c46fee484b1e4d1b3
Erick Rodriguez <erickrod54@gmail.com>
2023-12-16 21:30:00 -0500

    react-design-patterns-app - version 2.12 - leftSideComponentVariation - Features: Building Basic lefSideComponentVariation

---
+++ src/components/prop-sub-components/left.side.variation.prop.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**react-design-patterns-app - version 2.12 - LeftSideComponentVariation 
+ * - Features: 
+ * 
+ *     --> Building Basic 'LeftSideComponentVariation'
+ * 
+ * Note: This is the first topic
+ */
+
+
+const LeftSideComponentVariation = ({ title }) => {
+    return(
+        <h2>{ title }</h2>
+    )
+}
+
+export default LeftSideComponentVariation;
