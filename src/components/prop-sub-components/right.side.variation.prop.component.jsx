6afd9eb9c8dd671dbcfd1be35785aec8b1797746
Erick Rodriguez <erickrod54@gmail.com>
2023-12-16 21:30:00 -0500

    react-design-patterns-app - version 2.12 - prop-sub-components > index - Features: Importing and exporting LeftSideComponent and RightSideComponent variations

---
+++ src/components/prop-sub-components/right.side.variation.prop.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**react-design-patterns-app - version 2.12 - RightSideComponentVariation 
+ * - Features: 
+ * 
+ *     --> Building Basic 'RightSideComponentVariation'
+ * 
+ * Note: This is the first topic
+ */
+
+const RightSideComponentVariation = ({ title }) => {
+    return(
+        <h2>{ title }</h2>
+    )
+}
+
+export default RightSideComponentVariation;
