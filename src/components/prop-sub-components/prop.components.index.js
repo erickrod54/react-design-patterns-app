b5147ab9bc7ff0c59bc4dd2ae20e78393b4e852a
Erick Rodriguez <erickrod54@gmail.com>
2023-12-16 21:30:00 -0500

    react-design-patterns-app - version 2.12 - prop-sub-components > index - Fetaures: Importing and exporting leftSideComponent and RightSideComponet variations

--- src/components/prop-sub-components/prop.components.index.js
+++ src/components/prop-sub-components/prop.components.index.js
@@ -1,12 +1,16 @@
 import LeftSideComponent from "./left.side.prop.component";
+import LeftSideComponentVariation from "./left.side.variation.prop.component";
 import RightSideComponent from "./right.side.prop.component";
+import RightSideComponentVariation from "./right.side.variation.prop.component";
 
-/**react-design-patterns-app - version 2.05 - prop-sub-components > index 
+
+/**react-design-patterns-app - version 2.12 - prop-sub-components > index 
  * - Features: 
  * 
  *     --> Importing and exporting 'LeftSideComponent', and 'RightSideComponent' 
+ *         varitons
  * 
  * Note: This is the first topic
  */
 
-export { LeftSideComponent, RightSideComponent }
+export { LeftSideComponent, RightSideComponent, LeftSideComponentVariation, RightSideComponentVariation }
