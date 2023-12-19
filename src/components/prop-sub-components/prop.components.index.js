a40ac0dd192965f102ad72c655a2cd90be32f055
Erick Rodriguez <erickrod54@gmail.com>
2023-12-18 21:30:00 -0500

    react-design-patterns-app - version 2.15 - prop-sub-components > index - Features: Importing and exporting AuthorSmallListItems and AuthorLargeListItems

--- src/components/prop-sub-components/prop.components.index.js
+++ src/components/prop-sub-components/prop.components.index.js
@@ -1,16 +1,17 @@
+import AuthorLargeListItems from "./authors.large.items.component";
+import AuthorSmallListItems from "./autors.small.items.list.component";
 import LeftSideComponent from "./left.side.prop.component";
 import LeftSideComponentVariation from "./left.side.variation.prop.component";
 import RightSideComponent from "./right.side.prop.component";
 import RightSideComponentVariation from "./right.side.variation.prop.component";
 
-
-/**react-design-patterns-app - version 2.12 - prop-sub-components > index 
+/**react-design-patterns-app - version 2.15 - prop-sub-components > index 
  * - Features: 
  * 
- *     --> Importing and exporting 'LeftSideComponent', and 'RightSideComponent' 
+ *     --> Importing and exporting 'AuthorSmallListItems', and 'AuthorLargeListItems' 
  *         varitons
  * 
  * Note: This is the first topic
  */
 
-export { LeftSideComponent, RightSideComponent, LeftSideComponentVariation, RightSideComponentVariation }
+export { LeftSideComponent, RightSideComponent, LeftSideComponentVariation, RightSideComponentVariation, AuthorSmallListItems, AuthorLargeListItems }
