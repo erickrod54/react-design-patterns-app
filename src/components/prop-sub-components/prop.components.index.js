d48ad5af4aa8969b504fafc3b59f60f5728212ca
Erick Rodriguez <erickrod54@gmail.com>
2023-12-19 13:42:59 -0500

    react-design-patterns-app - version 2.16 - prop-sub-components > index - Features: Importing and exporting BookLargeListItems and BookSmallListItems

--- src/components/prop-sub-components/prop.components.index.js
+++ src/components/prop-sub-components/prop.components.index.js
@@ -1,17 +1,24 @@
 import AuthorLargeListItems from "./authors.large.items.component";
 import AuthorSmallListItems from "./autors.small.items.list.component";
+import BookLargeListItems from "./books.large.items.component";
+import BooksSmallListItems from "./books.small.items.component";
 import LeftSideComponent from "./left.side.prop.component";
 import LeftSideComponentVariation from "./left.side.variation.prop.component";
 import RightSideComponent from "./right.side.prop.component";
 import RightSideComponentVariation from "./right.side.variation.prop.component";
 
-/**react-design-patterns-app - version 2.15 - prop-sub-components > index 
+/**react-design-patterns-app - version 2.16 - prop-sub-components > index 
  * - Features: 
  * 
- *     --> Importing and exporting 'AuthorSmallListItems', and 'AuthorLargeListItems' 
- *         varitons
+ *     --> Importing and exporting 'BookLargeListItems', and 'BooksSmallListItems' 
  * 
  * Note: This is the first topic
  */
 
-export { LeftSideComponent, RightSideComponent, LeftSideComponentVariation, RightSideComponentVariation, AuthorSmallListItems, AuthorLargeListItems }
+export { LeftSideComponent,
+         RightSideComponent,
+         LeftSideComponentVariation,
+         RightSideComponentVariation,
+         AuthorSmallListItems,
+         AuthorLargeListItems,
+         BookLargeListItems, BooksSmallListItems }
