53ba27ba4ccea1ae0e470eda37cb3bee024265cb
Erick Rodriguez <erickrod54@gmail.com>
2023-12-21 21:30:00 -0500

    react-design-patterns-app - version 3.00 - styled.components - Features: Building OpenModalWrapper style component

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 2.20 - styled.components
+/**react-design-patterns-app - version 3.00 - styled.components
  * - Features: 
  * 
- *     --> Building 'ModalContent' style class
+ *     --> Building 'OpenModalWrapper' style component
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,18 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const OpenModalWrapper = styled.div`
+    img{
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+
+        &:hover{
+            background-color: aliceblue;
+        }
+    }
+`;
 
 export const ModalContent = styled.div`
     padding: 24px;
