cb7eacac93fce3d54c540829ba7f0f4e5ce031d6
Erick Rodriguez <erickrod54@gmail.com>
2023-12-22 13:30:01 -0500

    react-design-patterns-app - version 3.01 - assets > index - Features: Importing and exporting close asset

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,14 +1,15 @@
 import layoutexamples from './layout-examples.png'
 import sidebarexample from './side-bar-example.png'
 import clipboard from './copy.png'
+import close from './close_FILL0_wght400_GRAD0_opsz24.svg'
 
-/**react-design-patterns-app - version 2.10 - assets > index 
+/**react-design-patterns-app - version 3.01 - assets > index 
  * - Features: 
  * 
- *     --> importing and exporting 'clipboard' 
+ *     --> importing and exporting 'close' 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
  */
 
-export { layoutexamples, sidebarexample, clipboard }
+export { layoutexamples, sidebarexample, clipboard, close }
