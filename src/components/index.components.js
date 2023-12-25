38bf041391bd8b5dedb1fb6e59f056b0dab4c0c1
Erick Rodriguez <erickrod54@gmail.com>
2023-12-24 21:30:00 -0500

    react-design-patterns-app - version 3.06 - components > index - Features: Importing and exporting ContainerPattern

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,4 +1,5 @@
 import BashCode from "./bash.higlight.component";
+import ContainerPattern from "./container.component";
 import CustomTitle from "./custom.title.component";
 import IntroComponent from "./intro.component";
 import LayoutComponentPattern from "./layout.pattern.component";
@@ -7,12 +8,12 @@ import SharedLayout from "./shared.layout.component";
 import SplitScreen from "./split.screen.component";
 import SplitScreenVariation from "./split.screen.variation.component";
 
-/**react-design-patterns-app - version 2.13 - components > index  
+/**react-design-patterns-app - version 3.06 - components > index  
  * - Features: 
  * 
- *     --> Importing and exporting 'RegularList'
+ *     --> Importing and exporting 'ContainerPattern'
  * 
  * Note: Pending to make responsive design
  */
 
-export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent, SplitScreen, SplitScreenVariation, BashCode, RegularList}
+export { LayoutComponentPattern, SharedLayout, CustomTitle, IntroComponent, SplitScreen, SplitScreenVariation, BashCode, RegularList, ContainerPattern}
