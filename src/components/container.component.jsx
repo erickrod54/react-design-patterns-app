59fdc687ed2695f4e3710c64480b68942186cabb
Erick Rodriguez <erickrod54@gmail.com>
2023-12-24 21:30:00 -0500

    react-design-patterns-app - version 3.06 - ContainerPattern - Features: Building basic ContainerPattern Component

---
+++ src/components/container.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+/**react-design-patterns-app - version 3.06 - ContainerPattern
+ * - Features: 
+ * 
+ *     --> Building basic 'ContainerPattern'
+ * 
+ * Note: the cards components with the card information
+ * will be displayed here
+ */
+
+const ContainerPattern = () => {
+
+    return(
+        <h2>ContainerPattern</h2>
+    )
+}
+
+export default ContainerPattern;
