f400ea9913d43ed8bccfa7faf0ea7f3c38f815f9
Erick Rodriguez <erickrod54@gmail.com>
2023-12-30 22:54:54 -0500

    react-design-patterns-app - version 3.16 - CurrentUser - Features: Adding comments to the useEffect handler

--- src/components/current.user.component.jsx
+++ src/components/current.user.component.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-/**react-design-patterns-app - version 3.11 - CurrentUser 
+/**react-design-patterns-app - version 3.16 - CurrentUser 
  * - Features: 
  * 
- *     --> Building 'CurrentUser' pattern
+ *     --> Adding comments to the 'useEffect' handler
  * 
  * Note: the porpuse iod this component is to be the source
  * of data to multiple 'user' and 'books', and this
@@ -23,6 +23,7 @@ const CurrentUser = ({ children }) => {
     
     const [ user, setUser ] = useState(null)
 
+    /**is handling on mount the data mutation */
     useEffect(() => {
         (async () => {
             const response = await axios.get("/current-user");
