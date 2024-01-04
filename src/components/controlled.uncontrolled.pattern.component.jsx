035a209e3b549379c06ea1100e745a5f7b913908
Erick Rodriguez <erickrod54@gmail.com>
2024-01-04 14:02:41 -0500

    react-design-patterns-app - version 4.07 - ControlledUncontrolledPattern - Features: Developing uncontrolledcomponent exercise

--- src/components/controlled.uncontrolled.pattern.component.jsx
+++ src/components/controlled.uncontrolled.pattern.component.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import { CustomTitleStyle, SectionWrapper } from "../styledcomponents/styled.components";
+import React, { useState } from "react";
+import { ButtonGeneral, CustomTitleStyle, SectionWrapper } from "../styledcomponents/styled.components";
 import CustomTitle from "./custom.title.component";
 import UncontrolledPattern from "./uncontrolled.pattern.component";
+import BashCode from "./bash.higlight.component";
+import { usePatternsAppContext } from "../context";
+import ControlledPattern from "./controlled.pattern.component";
+import ControlledModal from "./modal.controlled.component";
 
-/**react-design-patterns-app - version 4.05 - ControlledUnControlledPattern
+/**react-design-patterns-app - version 4.07 - ControlledUnControlledPattern
  * - Features: 
  * 
- *     --> Develioping 'UnControlledPattern' concepts.
+ *     --> Developing 'uncontrolledcomponent' exercise
+ * 
+ *     --> Developing 'uncontrolledcomponent' exercise 
  * 
  * Note: this component will explain concepts of controlled and 
  * uncontrolled pattern
  */
 
 const ControlledUnControlledPattern = () => {
+
+    const [ displayModal, setDisplayModal ] = useState(false);
+    const { CodeData } = usePatternsAppContext();
+
+    const modalcode = CodeData[7].code;
+    const uncontrolledcomponent = CodeData[22].code;
+    const modalcontrolled = CodeData[23].code;
+    
+    const closeModal = () => setDisplayModal(false);
 
     return(
         <div>
@@ -91,6 +106,19 @@ const ControlledUnControlledPattern = () => {
 
         <UncontrolledPattern />
 
+        <p>
+            so the code for the form above is as follows:
+        </p>
+
+        <BashCode code={uncontrolledcomponent}/>
+
+        <p>
+            by not having explicit control over the state, due to the use of refs or 
+            direct manipulation of the DOM or a different use case, makes the form 
+            not than flexible ( but usefull for some use cases like searches results,
+            or maybe access level use cases and more )
+        </p>
+
         <CustomTitleStyle>
                 <CustomTitle  title={<p className="sub-title"><span >controlled component patterns</span></p>} />
         </CustomTitleStyle>
@@ -122,6 +150,43 @@ const ControlledUnControlledPattern = () => {
             the form elements and vice versa an use case can be Whenever the state changes, the UI updates accordingly. 
             Similarly, when the user interacts with the form element, it triggers an onChange event that updates the state.
         </p>
+
+        <p>
+            now lets see an example of a controlled pattern:
+        </p>
+
+        <ControlledPattern />
+
+        <p>
+            let check the modal code from the previous layout pattern, that also is an uncontrolled modal ( it is uncontrolled 
+            cause outside components cannot access directly to his state )
+        </p>
+
+        <BashCode code={modalcode}/>
+
+        <p>
+            so following this code if i want to control this component for example to close and open from outside of it,
+            it wont be possible unless i convert this uncontrolled pattern to a controlled pattern as follows:
+        </p>
+
+        <p>
+            by modifying the state handling will be like this:
+        </p>
+
+        <BashCode code={modalcontrolled}/>
+
+        <ButtonGeneral>
+                <button onClick={() => setDisplayModal(!displayModal)}>{displayModal ? 'Hide Modal' : 'Display modal'}</button>
+        </ButtonGeneral>   
+
+        <ControlledModal displayModal={displayModal} closeModal={closeModal}>
+                <h2>the modal content</h2>
+        </ControlledModal>
+
+        <p>
+           so the button that is outside of the component ControlledModal, is controlling the closing and opening 
+           behavior making it a controlled component
+        </p>        
 
         <p>
             the <span className="text-white font-semibold"> predictability </span> provides of this pattern
