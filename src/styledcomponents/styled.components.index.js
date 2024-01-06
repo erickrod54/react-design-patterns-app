a90a0443bd9485a484c919e4fe03885185ef8bc9
Erick Rodriguez <erickrod54@gmail.com>
2024-01-06 01:32:32 -0500

    react-design-patterns-app - version 4.08 - styledcomponents > index - Features: Importing and exporting StepFlowWrapper

--- src/styledcomponents/styled.components.index.js
+++ src/styledcomponents/styled.components.index.js
@@ -1,9 +1,9 @@
-import { ButtonGeneral, Container, CustomTitleStyle, FormWrapper, ListWrapper, ModalBackground, ModalContent, OpenModalWrapper, Panel, SectionWrapper } from "./styled.components";
+import { ButtonGeneral, Container, CustomTitleStyle, FormWrapper, ListWrapper, ModalBackground, ModalContent, OpenModalWrapper, Panel, SectionWrapper, StepFlowWrapper } from "./styled.components";
 
-/**react-design-patterns-app - version 4.06 - styledcomponents > index
+/**react-design-patterns-app - version 4.08 - styledcomponents > index
  * - Features: 
  * 
- *     --> Importing and exporting 'FormWrapper' 
+ *     --> Importing and exporting 'StepFlowWrapper' 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -14,4 +14,4 @@ import { ButtonGeneral, Container, CustomTitleStyle, FormWrapper, ListWrapper, M
 
 /**Sections and content */
 
-export { SectionWrapper, CustomTitleStyle, Container, Panel, ListWrapper, ModalBackground, ModalContent, OpenModalWrapper, ButtonGeneral, FormWrapper }
+export { SectionWrapper, CustomTitleStyle, Container, Panel, ListWrapper, ModalBackground, ModalContent, OpenModalWrapper, ButtonGeneral, FormWrapper, StepFlowWrapper }
