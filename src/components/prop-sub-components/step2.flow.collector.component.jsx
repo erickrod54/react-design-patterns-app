6889eae8a0311280385bb70abc1d32927997670a
Erick Rodriguez <erickrod54@gmail.com>
2024-01-07 13:13:01 -0500

    react-design-patterns-app - version 4.09 - Step2FlowCollector - Features: Changing Age by age to control data flow

--- src/components/prop-sub-components/step2.flow.collector.component.jsx
+++ src/components/prop-sub-components/step2.flow.collector.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ButtonGeneral, StepFlowWrapper } from "../../styledcomponents/styled.components";
 
-/**react-design-patterns-app - version 4.08 - Step2FlowCollector
+/**react-design-patterns-app - version 4.09 - Step2FlowCollector
  * - Features: 
  * 
- *     --> Building 'Step2FlowCollector' 
+ *     --> Changing 'Age' by 'age' to control the data flow  
  * 
  * Note: this step #2 will be part of the uncontrolled 
- * flow collector.
+ * flow collector and controlled flow, setting the base
+ * condition for this last one.
  */
 
 const Step2FlowCollector = ({ goNext }) => {
@@ -16,7 +17,7 @@ const Step2FlowCollector = ({ goNext }) => {
         <StepFlowWrapper>
             <h2>the Step #2: Enter your age: </h2>
             <ButtonGeneral>
-                <button onClick={() => goNext({ Age: 39 })}>Next</button>
+                <button onClick={() => goNext({ age: 39 })}>Next</button>
             </ButtonGeneral>
         </StepFlowWrapper>
     )
