e42a30f42dad55f0dabc627015e7dff575c36e1a
Erick Rodriguez <erickrod54@gmail.com>
2024-01-22 00:56:35 -0500

    react-design-patterns-app - version 5.13 - ParentComponent - Features: Building ParentComponent

---
+++ src/components/parent.observer.pattern.component.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Buttons from "./prop-sub-components/prop.components.index"
+
+/**react-design-patterns-app - version 5.13 - ParentComponent  
+ * - Features: 
+ * 
+ *     --> Building 'ParentComponent'
+ * 
+ * Note: Pending to make responsive design
+ */
+
+const ParentComponent = () => {
+
+    return(
+        <>
+            <Buttons />
+            {/**<Counter /> */}
+        </>
+    )
+}
+
+export default ParentComponent;
