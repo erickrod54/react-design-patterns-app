f770ee34b2db39577bb73596057aa0eeb1dfee12
Erick Rodriguez <erickrod54@gmail.com>
2024-01-22 00:45:21 -0500

    react-design-patterns-app - version 5.13 - assets > index - Features: importing and exporting remove

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -4,14 +4,15 @@ import clipboard from './copy.png'
 import close from './close_FILL0_wght400_GRAD0_opsz24.svg'
 import openmodal from './circle_FILL0_wght400_GRAD0_opsz24.svg'
 import add from './add_FILL0_wght400_GRAD0_opsz24.png'
+import remove from './remove_FILL0_wght400_GRAD0_opsz24 (1).png'
 
-/**react-design-patterns-app - version 5.12 - assets > index 
+/**react-design-patterns-app - version 5.13 - assets > index 
  * - Features: 
  * 
- *     --> importing and exporting 'add' 
+ *     --> importing and exporting 'remove' 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
  */
 
-export { layoutexamples, sidebarexample, clipboard, close, openmodal, add }
+export { layoutexamples, sidebarexample, clipboard, close, openmodal, add, remove }
