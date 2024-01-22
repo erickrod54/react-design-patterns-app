3e06c60403bce86f306a408fb9b0d36a63f0ab09
Erick Rodriguez <erickrod54@gmail.com>
2024-01-21 21:30:00 -0500

    react-design-patterns-app - version 5.12 - assets > index - Features: importing and exporting add

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -3,14 +3,15 @@ import sidebarexample from './side-bar-example.png'
 import clipboard from './copy.png'
 import close from './close_FILL0_wght400_GRAD0_opsz24.svg'
 import openmodal from './circle_FILL0_wght400_GRAD0_opsz24.svg'
+import add from './add_FILL0_wght400_GRAD0_opsz24.png'
 
-/**react-design-patterns-app - version 3.02 - assets > index 
+/**react-design-patterns-app - version 5.12 - assets > index 
  * - Features: 
  * 
- *     --> importing and exporting 'openmodal' 
+ *     --> importing and exporting 'add' 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
  */
 
-export { layoutexamples, sidebarexample, clipboard, close, openmodal }
+export { layoutexamples, sidebarexample, clipboard, close, openmodal, add }
