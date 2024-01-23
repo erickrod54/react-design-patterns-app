7622fa8e323fe215d411692bca623e8bac8dc4b3
Erick Rodriguez <erickrod54@gmail.com>
2024-01-23 02:08:13 -0500

    react-design-patterns-app - version 5.15 - Counter - Features: Implementing emitter.on and emitter.off

--- src/components/prop-sub-components/counter.observer.pattern.component.jsx
+++ src/components/prop-sub-components/counter.observer.pattern.component.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { emitter } from "../functional.programming.component";
 
-/**react-design-patterns-app - version 5.13 - Counter  
+/**react-design-patterns-app - version 5.15 - Counter  
  * - Features: 
  * 
- *     --> Building 'Counter' Compnent
+ *     --> Implementing 'emitter.on' and 
+ *         'emitter.off' 
  * 
- * Note: Pending to make responsive design
+ * Note: with emitter on and off methods i am 
+ * listening an event and i am turning off the 
+ * listener once is done
  */
 
 const Counter = () => {
-    const [ count, setCount ] = useState(0)
+    const [ count, setCount ] = useState(0);
+
+    useEffect(() => {
+        /**here i create simple handlers */
+        const onIncrement = () => {
+            setCount((count) => count + 1)
+        }
+        const onDecrement = () => {
+            setCount((count) => count - 1)
+        }
+        /**here is listening / observing the increment
+         * and decrement*/
+        emitter.on('inc', onIncrement)
+        emitter.on('dec', onDecrement)
+
+        return () => {
+            /**once observes .. it turns off */
+            emitter.off('inc', onIncrement)
+            emitter.off('dec', onDecrement)
+        }
+    },[])
 
     return <div>#: {count}</div>
 }
