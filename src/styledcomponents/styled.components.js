426ae332f6e0d0e75a66b2abfde5751bacbabf45
Erick Rodriguez <erickrod54@gmail.com>
2024-01-25 01:23:55 -0500

    react-design-pattern-app - version 5.17 - styled.components - Features: writting advanced-hooks-portals-cases style class

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 5.16 - styled.components
+/**react-design-patterns-app - version 5.17 - styled.components
  * - Features: 
  * 
- *     --> writting 'AlertWrapper' style component.	
+ *     --> writting '.advanced-hooks-portals-cases' style class.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -184,6 +184,12 @@ export const CustomTitleStyle = styled.div`
  .advanced-hooks-portals{
 	span{
 			${tw`text-blue-500 underline`}
+		}
+ }
+
+ .advanced-hooks-portals-cases{
+	span{
+			${tw`text-blue-400 underline`}
 		}
  }
 
