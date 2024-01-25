868d13c968763aca5035319f6400f1ee5416d8fc
Erick Rodriguez <erickrod54@gmail.com>
2024-01-25 01:31:38 -0500

    react-design-patterns-app - version 5.17 - asset > index - Features: replacing add asset

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -3,13 +3,13 @@ import sidebarexample from './side-bar-example.png'
 import clipboard from './copy.png'
 import close from './close_FILL0_wght400_GRAD0_opsz24.svg'
 import openmodal from './circle_FILL0_wght400_GRAD0_opsz24.svg'
-import add from './add_FILL0_wght400_GRAD0_opsz24.png'
+import add from './add_FILL0_wght400_GRAD0_opsz24.svg'
 import remove from './remove_FILL0_wght400_GRAD0_opsz24 (1).png'
 
-/**react-design-patterns-app - version 5.13 - assets > index 
+/**react-design-patterns-app - version 5.17 - assets > index 
  * - Features: 
  * 
- *     --> importing and exporting 'remove' 
+ *     --> replacing 'add' asset 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
