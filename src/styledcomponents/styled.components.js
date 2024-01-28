55bc1dd3f5284577c46cebe461736354d72554a9
Erick Rodriguez <erickrod54@gmail.com>
2024-01-27 21:30:00 -0500

    react-deisgn-patterns-app - 6.03 - styled.components - Features: Writting a styles class

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 5.17 - styled.components
+/**react-design-patterns-app - version 6.03 - styled.components
  * - Features: 
  * 
- *     --> writting '.advanced-hooks-portals-cases' style class.	
+ *     --> writting 'a' style class.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -116,6 +116,15 @@ export const SectionWrapper = styled.section`
     p{
         ${tw`mt-6 mb-8 first-letter:capitalize`}
     }
+
+	a{
+		${tw`text-blue-200`}
+
+	}
+	
+	a:hover{
+		${tw`text-blue-400`}
+	}
 
     `
 
