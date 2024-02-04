8881b085e1f13fb4388ca53ff067c09dd4b25767
Erick Rodriguez <erickrod54@gmail.com>
2024-02-03 23:06:56 -0500

    react-design-patterns-app - version 7.00 - data js - Features: Adding includeUpdatableResource HOC code

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
-/**react-design-patterns-app - version 6.20 - data js  
+/**react-design-patterns-app - version 7.00 - data js  
  * - Features: 
  *    
- *     --> Adding 'includeUpdatableUser' 
+ *     --> Adding 'includeUpdatableResource' 
  *        component code
  * 
  * Note: This component will have later the main menu
@@ -1576,6 +1576,64 @@ export const CodeData = [
     };
     
     export default includeUpdatableUser;
+  `
+  },
+  {
+    id: 51,
+    name: 'includeUpdatableResource - HOCPattern',
+    code:   
+    `
+/**this utility is made in order to capitalize the resource name */
+const toCapital = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+/**here i pass 'resourceUrl' that will be the custom path of the resource, 
+ * 'resourceName' that will be the name of the dataset*/
+    const includeUpdatableResource = (Component, resourceUrl, resourceName) => {
+      return (props) => {
+        const [ intialResource, setInitialResource ] = useState(null);
+        const [ resource, setResource ] = useState(null);
+
+        useEffect(() => {
+          (async () => {
+            const response = await axios.get(resourceUrl);
+            setInitialResource(response.data);
+            setResource(response.data);
+          })();
+        }, []);
+
+        const onChange = (updates) => {
+            setResource({ ...resource, ...updates });
+        };
+
+        const onPost = async () => {
+          const response = await axios.post(resourceUrl, {
+            [resourceName]: resource,
+          });
+          setInitialResource(response.data);
+          setResource(response.data);
+        };
+
+        const onReset = () => {
+          setResource(intialResource);
+        };
+
+        /**this solution is made in order to overcome the get and post
+         * of several resources from different dataset, due to a high 
+         * number of sitautions where can exist conclict by the props names*/
+        const resourceProps = {
+            [resourceName]: resource,
+            [\`onChange\${toCapital(resourceName)}\`]: onChange,
+            [\`onPost\${toCapital(resourceName)}\`]: onPost,
+            [\`onReset\${toCapital(resourceName)}\`]: onReset,
+        }
+
+        return (
+          <Component {...props} {...resourceProps}/>
+        );
+      };
+    };
+
+    export default includeUpdatableResource;
   `
   }
 ];
