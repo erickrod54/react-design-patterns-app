ca9877e16cb2894555319bbfaab3a73c862604d4
Erick Rodriguez <erickrod54@gmail.com>
2024-02-09 21:30:00 -0500

    react-design-patterns-app - version 7.18 - styled.components - Features: witting forwarding style class

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 7.17 - styled.components
+/**react-design-patterns-app - version 7.18 - styled.components
  * - Features: 
  * 
- *     --> writting 'portal-end' style class.	
+ *     --> writting 'forwarding-refs' style class.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -211,6 +211,12 @@ export const CustomTitleStyle = styled.div`
  .advanced-hooks-portals-cases{
 	span{
 			${tw`text-blue-400 underline`}
+		}
+ }
+
+ .forwarding-refs{
+	span{
+			${tw`text-amber-400 underline`}
 		}
  }
 
