57a858eb41a4d85aace5967505abfe6d7a608520
Erick Rodriguez <erickrod54@gmail.com>
2024-02-10 21:30:00 -0500

    react-design-patterns-app - version 7.19 - ForwardingRefs - Features: Developing ForwardingRefs example

--- src/components/forwarding.refs.component.jsx
+++ src/components/forwarding.refs.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CustomTitle from "./custom.title.component";
 import { CustomTitleStyle, SectionWrapper } from "../styledcomponents/styled.components";
+import BashCode from "./bash.higlight.component";
+import { usePatternsAppContext } from "../context";
 
-/**react-design-patterns-app - version 7.18 - ForwardingRefs 
+/**react-design-patterns-app - version 7.19 - ForwardingRefs 
  * - Features: 
  * 
- *     --> Building 'ForwardingRefs' component. 
+ *     --> Developing 'ForwardingRefs' example. 
  * 
  * Note: 'UserInfoFormAnyResource' taking as base code the
  * 'UserInfoForm', with few modifications can render any 
@@ -15,6 +17,10 @@ import { CustomTitleStyle, SectionWrapper } from "../styledcomponents/styled.com
 
 const ForwardingRefs = () => {
 
+    const { CodeData } = usePatternsAppContext();
+
+    const forwardingrefformcode = CodeData[62].code;
+
     return(
         <div>
         <CustomTitleStyle>
@@ -22,6 +28,28 @@ const ForwardingRefs = () => {
         </CustomTitleStyle>
 
         <SectionWrapper>
+            <p>
+            <span className="text-white font-semibold">references or Refs </span> is widely use in react in order to provide multiple solutions related with 
+                <span className="text-white font-semibold"> access DOM elements, integrating third 
+                party libraries, managing form data ( about the example for this section ), dynamic 
+                content updates, optimizing performance, working with legacy code </span> 
+            </p>
+
+            <p>
+                in this case let focus on <span className="text-white font-semibold"> managing form data </span>, 
+                this is made in order to achieve multiple goals for example to handle <span className="text-white font-semibold"> 
+                user input, or performing form validation or simply to acces for values directly simplifying form handling logic 
+                 ( about this example of data managing ) </span>
+            </p>
+
+            <p>
+                so i have the following <span className="text-white font-semibold"> form </span> in a react component, and i am 
+                <span className="text-white font-semibold"> accessing </span> to the <span className="text-white font-semibold"> 
+                user input </span> using a <span className="text-white font-semibold"> useRef hook </span> form directly in 
+                this component:
+            </p>
+
+            <BashCode  code={forwardingrefformcode} />
 
         </SectionWrapper>
         
