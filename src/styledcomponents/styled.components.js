75913c59fb930eb3afab537476707c2a4898e9b7
Erick Rodriguez <erickrod54@gmail.com>
2024-02-11 21:30:00 -0500

    react-design-patterns-app - version 8.01 - styled.components - Features: writting error-boundaries style class

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 7.18 - styled.components
+/**react-design-patterns-app - version 8.01 - styled.components
  * - Features: 
  * 
- *     --> writting 'forwarding-refs' style class.	
+ *     --> writting 'error-boundaries' style class.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -217,6 +217,12 @@ export const CustomTitleStyle = styled.div`
  .forwarding-refs{
 	span{
 			${tw`text-amber-400 underline`}
+		}
+ }
+
+ .error-boundaries{
+	span{
+			${tw`text-red-400 underline`}
 		}
  }
 
