809b2da9247311fbb2c31cbcaddd33ce0659578d
Erick Rodriguez <erickrod54@gmail.com>
2024-02-13 21:30:00 -0500

    react-design-patterns-app - version 8.07 - ChildComponent - Features: Building ChildComponent

---
+++ src/components/error.boundaries.child.component.jsx
@@ -0,0 +1,20 @@
+import  React from 'react';
+
+/**react-design-patterns-app - version 8.07 - ChildComponent
+ * - Features: 
+ * 
+ *     --> Building 'ChildComponent'
+ * 
+ * Note: the cards components with the card information
+ * will be displayed here
+ */
+
+const ChildComponent = () => {
+
+    //throw new Error("Error in component")
+    return(
+        <h2>Child Component</h2>
+    )
+}
+
+export default ChildComponent;
