e4c8e41cb20b16b8309d649a99984368ee32677f
Erick Rodriguez <erickrod54@gmail.com>
2024-02-14 21:30:00 -0500

    react-design-patterns-app - version 8.09 - styledcomponents > index - Features: Importing and exporting ChildWrapper

--- src/styledcomponents/styled.components.index.js
+++ src/styledcomponents/styled.components.index.js
@@ -1,9 +1,9 @@
-import { AlertWrapper, ButtonGeneral, ButtonWrapper, Container, CustomTitleStyle, FormWrapper, ListWrapper, MasterWrapper, ModalBackground, ModalContent, OpenModalWrapper, Panel, SectionWrapper, StepFlowWrapper } from "./styled.components";
+import { AlertWrapper, ButtonGeneral, ButtonWrapper, ChildWrapper, Container, CustomTitleStyle, FormWrapper, ListWrapper, MasterWrapper, ModalBackground, ModalContent, OpenModalWrapper, Panel, SectionWrapper, StepFlowWrapper } from "./styled.components";
 
-/**react-design-patterns-app - version 8.08 - styledcomponents > index
+/**react-design-patterns-app - version 8.09 - styledcomponents > index
  * - Features: 
  * 
- *     --> Importing and exporting 'MasterWrapper' 
+ *     --> Importing and exporting 'ChildWrapper' 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -19,4 +19,4 @@ export { SectionWrapper,
          Container,
          Panel, ListWrapper,
          ModalBackground,
-         ModalContent, OpenModalWrapper, ButtonGeneral, FormWrapper, StepFlowWrapper, ButtonWrapper, AlertWrapper, MasterWrapper }
+         ModalContent, OpenModalWrapper, ButtonGeneral, FormWrapper, StepFlowWrapper, ButtonWrapper, AlertWrapper, MasterWrapper, ChildWrapper }
