79105b90499b868fd047bb119f327f5be9f0b8a1
Erick Rodriguez <erickrod54@gmail.com>
2024-02-15 21:30:00 -0500

    react-design-patterns-app - version 8.10 - MasterComponent - Features: Renaming h1 tag to Master Component

--- src/components/error.boundaries.parent.component.jsx
+++ src/components/error.boundaries.parent.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ChildComponent from "./error.boundaries.child.component";
 import { ChildWrapper, MasterWrapper } from "../styledcomponents/styled.components";
 
-/**react-design-patterns-app - version 8.09 - MasterComponent
+/**react-design-patterns-app - version 8.10 - MasterComponent
  * - Features: 
  * 
- *     --> Adding 'ChildWrapper' style component
+ *     --> Renaming h1 tag to 'Master Component'
  * 
  * Note: the cards components with the card information
  * will be displayed here
@@ -15,7 +15,7 @@ const MasterComponent = () => {
     
     return (
         <MasterWrapper>
-            <h1>Parent Component</h1>
+            <h1>Master Component</h1>
             <ChildWrapper>
                 <ChildComponent />
             </ChildWrapper>
