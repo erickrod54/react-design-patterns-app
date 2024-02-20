1173ed22dfc63783cf0cfdfda00df9e0b93e210a
Erick Rodriguez <erickrod54@gmail.com>
2024-02-19 21:30:00 -0500

    react-design-patterns-app - version 9.03 - AdavncedHooks - Features: listing advanced hooks and small descriptions

--- src/components/advanced.hooks.component.jsx
+++ src/components/advanced.hooks.component.jsx
@@ -9,10 +9,10 @@ import UserInfoDataSource from "./user.info.hook.dsource.component";
 import { usePatternsAppContext } from "../context";
 import BashCode from "./bash.higlight.component";
 
-/**react-design-patterns-app - version 9.02 - AdvancedHooks
+/**react-design-patterns-app - version 9.03 - AdvancedHooks
  * - Features: 
  * 
- *     --> Building 'AdvancedHooks'  
+ *     --> listing advanced hooks and small description  
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
@@ -29,8 +29,40 @@ const AdvancedHooks = () => {
             <SectionWrapper>
 
                 <p>
-                    this will be a compilation of a wide advanced hooks patterns used to solve a variaty of problems:
+                    this will be a <span className="text-white font-semibold"> compilation 9 most use advanced hooks patterns </span> 
+                    used to solve a variaty of problems:
                 </p>
+
+            <ul>
+                <li>
+                    <span className="text-white font-semibold"> keys </span> ( Unique identifiers used by React to efficiently update and manage lists of elements )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> event listeners </span> ( Functions that listen for specific events - such as clicks or key presses - and execute code in response to those events )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useLayoutEffect </span> ( A React hook similar to `useEffect`, but fires synchronously after all DOM mutations. )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useId </span> ( Custom React hook used to generate unique IDs for elements, often used in conjunction with accessibility or for DOM manipulation. )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useCallback as Refs </span> ( React hook used to memoize callback functions, commonly used with `useRef` to maintain consistent references between renders.)
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useImperativeHandle </span> ( React hook used to customize the instance value that is exposed when using `ref` with `forwardRef`)
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useDeferredValue </span> ( React hook that defers the update of a value to avoid blocking the UI, useful for optimizing performance of non-critical updates.)
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useTransition </span> ( React hook that allows for smooth transitions when adding or removing elements from the DOM, improving user experience)
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> Async React Router </span> ( Feature of React Router that allows for lazy loading and code splitting of route components, enhancing performance by loading components asynchronously. )
+                </li>
+            </ul>
+
             </SectionWrapper>
         </div>)
 
