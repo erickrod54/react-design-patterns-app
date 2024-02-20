7adf4635cf9f27121328c977ccaf1315ba8cbad5
Erick Rodriguez <erickrod54@gmail.com>
2024-02-19 21:30:00 -0500

    react-design-patterns-app - version 9.02 - styled.components - Features: writting advanced-hooks-patterns style class

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 8.08 - styled.components
+/**react-design-patterns-app - version 9.02 - styled.components
  * - Features: 
  * 
- *     --> writting 'ChildWrapper' style class.	
+ *     --> writting 'advanced-hooks-patterns' style class.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -230,6 +230,12 @@ export const CustomTitleStyle = styled.div`
  .error-boundaries{
 	span{
 			${tw`text-red-400 underline`}
+		}
+ }
+
+ .advanced-hooks-patterns{
+	span{
+			${tw`text-violet-400 underline`}
 		}
  }
 
