d621fc41d52e19bf4effa84107530d25dfcb410a
Erick Rodriguez <erickrod54@gmail.com>
2024-02-26 13:42:07 -0500

    react-desing-patterns-app - version 9.20 - styled.components - Features: writting event-listeners-hooks text styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 9.11 - styled.components
+/**react-design-patterns-app - version 9.20 - styled.components
  * - Features: 
  * 
- *     --> writting 'KeyExerciseWrapper' text styles.	
+ *     --> writting 'event-listeners-hooks' text styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -267,6 +267,11 @@ export const CustomTitleStyle = styled.div`
 		}
  }
 
+.event-listeners-hooks{
+	span{
+			${tw`text-violet-200 underline`}
+		}
+}
 `
 
 export const Container = styled.div`
