6a558d58bbdff095518cb94247cb9855c502c08b
Erick Rodriguez <erickrod54@gmail.com>
2024-03-07 21:30:00 -0500

    react-design-patterns-app - version 11.06 - styled.components - Features: writting FormWrapper new styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 11.03 - styled.components
+/**react-design-patterns-app - version 11.06 - styled.components
  * - Features: 
  * 
- *     --> writting 'useId-hook-pattern' styles.	
+ *     --> writting 'FormWrapper' new styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -83,9 +83,12 @@ export const KeyExerciseWrapper = styled.div`
 
 
 export const FormWrapper = styled.div`
+	${tw`bg-violet-500 m-4 p-4 rounded-2xl`}
+
 	input{
-		${tw`flex justify-center items-center m-4 p-4 rounded text-black`}
+		${tw`flex justify-center items-center m-4 p-4 rounded text-black border-4 border-amber-600`}
 	}
+
 `
 
 export const AlertWrapper = styled.div`
