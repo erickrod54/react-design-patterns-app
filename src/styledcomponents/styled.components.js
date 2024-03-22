7ccdebde4089bf76997c5fbe83f42e7d6f9204ca
Erick Rodriguez <erickrod54@gmail.com>
2024-03-22 02:55:58 -0400

    react-design-patterns-app - version 13.08 - styled.components - Features: Writting .use-imperative-handle-hook styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 12.12 - styled.components
+/**react-design-patterns-app - version 13.08 - styled.components
  * - Features: 
  * 
- *     --> Building 'use-callback-hook-as-refs' styles.	
+ *     --> Writting '.use-imperative-handle-hook' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -310,6 +310,12 @@ export const CustomTitleStyle = styled.div`
 .use-callback-hook-as-refs{
 	span{
 			${tw`text-amber-200 underline`}
+		}
+}
+
+.use-imperative-handle-hook{
+	span{
+			${tw`text-amber-300 underline`}
 		}
 }
 
