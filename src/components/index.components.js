79498073ac2e778cd0303c81f8c77f7ddba1ab52
Erick Rodriguez <erickrod54@gmail.com>
2024-03-28 03:05:15 -0400

    react-design-patterns-app - version 14.03 - components > index - Features: Importing and exporting UseImperativeMultipleFocus

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -59,6 +59,7 @@ import UseImperativeHookPattern from "./use.imperative.hook.before.component";
 import UseWithImperativeFocusHookPattern from "./use.imperative.hook.focus.after.component";
 import UseImperativeFocusHookPattern from "./use.imperative.hook.focus.before.component";
 import UseWithImperativeFocusMethodHookPattern from "./use.imperative.hook.focus.method.component";
+import UseImperativeMultipleFocus from "./use.imperative.hook.focus.multiple.component";
 import CallbackAsRefAfter from "./usecallback.as.a.ref.after.component";
 import CallbackAsRefBefore from "./usecallback.as.a.ref.before.component";
 import CallbackAsRefCommentedBefore from "./usecallback.as.a.ref.commented.before.component";
@@ -76,10 +77,10 @@ import UserInfoDataSource from "./user.info.hook.dsource.component";
 import WithoutKeysExercise from "./withoutkeys.exersice.component";
 import WithtKeysExercise from "./withtkeys.exersice.component";
 
-/**react-design-patterns-app - version 14.02 - components > index  
+/**react-design-patterns-app - version 14.03 - components > index  
  * - Features: 
  * 
- *     --> Importing and exporting 'ForwardingImperativeMultipleFocus'
+ *     --> Importing and exporting 'UseImperativeMultipleFocus'
  * 
  * Note: Pending to make responsive design
  */
@@ -162,5 +163,6 @@ export {
          UseWithImperativeFocusHookPattern,
          ForwardingImperativeFocusRefInput,
          UseWithImperativeFocusMethodHookPattern,
-         ForwardingImperativeMultipleFocus
+         ForwardingImperativeMultipleFocus,
+         UseImperativeMultipleFocus
         }
