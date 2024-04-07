957f289245e8f1ebc50aa4eb3de71c566c54b6da
Erick Rodriguez <erickrod54@gmail.com>
2024-04-06 21:30:00 -0400

    react-design-patterns-app - version 15.02 - data js - Features: Adding UseDeferredAfterHook code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 15.00 - data js  
+/**react-design-patterns-app - version 15.02 - data js  
  * - Features: 
  *    
- *     --> Adding 'UseDeferredAfterMemo'  code comments
+ *     --> Adding 'UseDeferredAfterHook'  code
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -3225,6 +3225,41 @@ const toCapital = str => str.charAt(0).toUpperCase() + str.slice(1);
       }
       
       export default UseDeferredAfterMemo;
+      `
+    },
+    {
+    id: 108,
+    name: 'UseDeferredAfterHook - Use Deferred Value Hook Pattern',
+    code:   
+    `
+    const UseDeferredAfterHook = () => {
+
+      const id = useId();
+  
+      const [ keyword, setKeyword ] = useState("")
+  
+      const deferredKeyword = useDeferredValue(keyword)
+  
+      const handleChange = (e) => {
+          e.preventDefault();
+  
+          setKeyword(e.target.value)
+      }
+    
+          return(
+              <FormWrapperDeferred>
+                  <label htmlFor={\`\${id}-type\`}>Type here your entry</label>
+                  <input 
+                      id={\`\${id}-type\`}
+                      value={keyword}
+                      onChange={handleChange}
+                  />
+                  <HeavyComponentAfterFirst keyword={deferredKeyword}/>
+              </FormWrapperDeferred>
+          )
+      }
+      
+      export default UseDeferredAfterHook;
       `
     } 
   ];
