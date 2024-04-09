305e35dec4e730349d22726104ff359de5ef56f8
Erick Rodriguez <erickrod54@gmail.com>
2024-04-08 21:30:00 -0400

    react-design-patterns-app - version 15.05 - writer - Features: Building Writer Component

---
+++ src/components/writer.use.transition.before.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+/**react-design-patterns-app - version 15.05 - Writer
+ * - Features: 
+ * 
+ *     --> Building 'Writer' component
+ * 
+ * Note: the writter component for the useTransition 
+ * pattern  
+ */
+
+const Writer = () => {
+  return (
+    <>
+        <p>
+            Codelicks Academy
+        </p>
+    </>
+  )
+};
+
+export default Writer;
