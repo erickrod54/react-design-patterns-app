83d62f90721c5a91ae215a6b9f04a5d5c41c873f
Erick Rodriguez <erickrod54@gmail.com>
2024-04-08 21:30:00 -0400

    react-design-patterns-app - version 15.05 - Cover - Features: Building Cover component

---
+++ src/components/cover.use.transition.before.hook.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+/**react-design-patterns-app - version 15.05 - Cover
+ * - Features: 
+ * 
+ *     --> Building 'Cover' component
+ * 
+ * Note: the Cover component for the useTransition 
+ * pattern  
+ */
+
+const Cover = () => {
+  return (
+    <>
+      <>
+        📚
+      </>
+    </>
+  );
+};
+
+export default Cover;
