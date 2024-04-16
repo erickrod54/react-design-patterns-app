39bd6da6d2c8c7f8c48779f004d58aa2cf3c81d7
Erick Rodriguez <erickrod54@gmail.com>
2024-04-15 21:30:00 -0400

    react-design-patterns-app - version 16.04 - styled.components - Features: Writing ButtonUseTransition second tailwind styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 16.03 - styled.components
+/**react-design-patterns-app - version 16.04 - styled.components
  * - Features: 
  * 
- *     --> Writing 'ButtonUseTransition' first tailwind styles.	
+ *     --> Writing 'ButtonUseTransition' second tailwind styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -31,9 +31,9 @@ export const CoverContainer = styled.div`
 
 export const ButtonUseTransition = styled.button`
   ${tw`bg-gray-200 border border-gray-300 px-6 py-4 mx-5 cursor-pointer rounded-lg text-base transition duration-300 ease-in-out`}
-
+  
   &:hover {
-    background-color: #ddd;
+	  ${tw`bg-gray-400`}
   }
 
   &:focus {
