e09a397e5709ce8c262efba5db715a50c41275d9
Erick Rodriguez <erickrod54@gmail.com>
2024-04-16 21:30:00 -0400

    react-design-patterns-app - version 16.08 - styled.components - Features: Writing BooksUseTransition container div tailwind styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 16.07 - styled.components
+/**react-design-patterns-app - version 16.08 - styled.components
  * - Features: 
  * 
- *     --> Writing 'Emoji' tailwind styles.	
+ *     --> Writing 'BooksUseTransitionContainer' div tailwind styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -56,7 +56,11 @@ export const MasterWrapper = styled.div`
 `
 
 export const BooksUseTransitionContainer = styled.div`
-	${tw`flex flex-row text-black bg-violet-500 m-4 p-4 rounded-2xl h-fit w-fit`}
+	${tw`flex flex-col text-black bg-violet-500 m-4 p-4 rounded-2xl h-fit w-fit`}
+
+	div{
+		${tw`flex flex-row m-4 p-4`}
+	}
 `
 
 export const ChildWrapper = styled.div`
