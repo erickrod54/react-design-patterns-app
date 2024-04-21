31e8437c72be02c89b2a89b4402073148e6e2bfd
Erick Rodriguez <erickrod54@gmail.com>
2024-04-20 21:30:00 -0400

    react-design-patterns-app - version 16.17 - styled.components - Features: writing as-prop-clean-code-tip styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 16.16 - styled.components
+/**react-design-patterns-app - version 16.17 - styled.components
  * - Features: 
  * 
- *     --> Writing 'clean-code-tips' styles.	
+ *     --> Writing 'as-prop-clean-code-tip' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -412,6 +412,12 @@ export const CustomTitleStyle = styled.div`
 .clean-code-tips{
 	span{
 			${tw`text-cyan-300 underline`}
+		}
+}
+
+.as-prop-clean-code-tip{
+	span{
+			${tw`text-violet-300 underline`}
 		}
 }
 
