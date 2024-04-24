2c6f8f98478706b8ef767194254c223262672767
Erick Rodriguez <erickrod54@gmail.com>
2024-04-23 21:30:00 -0400

    react-design-patterns-app - version 17.04 - ButtonAsCleanCodeAppAfter - Features: Refactoring href to /21

--- src/components/buttons.as.clean.code.tip.after.app.component.jsx
+++ src/components/buttons.as.clean.code.tip.after.app.component.jsx
@@ -1,9 +1,9 @@
 import ButtonAsCleanCode from "./button.as.clean.code.tip.component";
 
-/**react-design-patterns-app - version 17.03 - ButtonAsCleanCodeAppAfter
+/**react-design-patterns-app - version 17.04 - ButtonAsCleanCodeAppAfter
  * - Features: 
  * 
- *     --> Building 'ButtonAsCleanCodeAppAfter'
+ *     --> Refactoring href to "/21"
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
@@ -24,7 +24,7 @@ const ButtonAsCleanCodeAppAfter = () => {
       <ButtonAsCleanCode size="m">Medium</ButtonAsCleanCode>
       <ButtonAsCleanCode size="l">Large</ButtonAsCleanCode>
       <ButtonAsCleanCode size="xl">xLarge</ButtonAsCleanCode>
-      <a href="/">Link</a>
+      <a href="/21">Link</a>
     </div>
   );
 }
