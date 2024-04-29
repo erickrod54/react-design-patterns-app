6ec5e3e8e2aa4bc025b1a88e12b87c515c2e354b
Erick Rodriguez <erickrod54@gmail.com>
2024-04-28 21:30:00 -0400

    react-deisgn-patterns-app - version 17.16 - styles.components - Features: Writting ButtonOptContextStyle styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 16.17 - styled.components
+/**react-design-patterns-app - version 17.16 - styled.components
  * - Features: 
  * 
- *     --> Writing 'as-prop-clean-code-tip' styles.	
+ *     --> Writing 'ButtonsOptContextStyle' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,10 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const ButtonsOptContextStyle = styled.span`
+  ${tw`text-center mt-14`}
+`;
 
 export const Emoji = styled.span`
   	${tw`text-5xl flex`}
