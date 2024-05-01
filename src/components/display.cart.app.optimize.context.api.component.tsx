bdbadcc1d57287b7fd351a6dcddb877e6912548f
Erick Rodriguez <erickrod54@gmail.com>
2024-05-01 03:40:28 -0400

    react-design-patterns-app - version 18.01 - Display - Features: Building Display Component

--- src/components/display.cart.app.optimize.context.api.component.tsx
+++ src/components/display.cart.app.optimize.context.api.component.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { useCartContext } from "./shopping.cart.app.optimize.context.api.component.tsx";
 
+/**react-design-patterns-app - version 18.01 - Display
+ * - Features: 
+ * 
+ *     --> Building 'Display' Component 
+ *
+ * Note: 'UserInfo' with a few modifications ends up in 
+ * 'UserInfoWithHook'  
+*/
+
 const Display = () => {
   const {
     state: { count },
