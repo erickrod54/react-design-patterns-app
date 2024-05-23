8bf6df23f914c9138f40d293ae15b00ac522c841
Erick Rodriguez <erickrod54@gmail.com>
2024-05-22 21:30:00 -0400

    react-design-patterns-app - version 20.18 - UsersApiStatesHelper - Features: Building UsersApiStatesHelper

---
+++ src/components/user.api.handling.helper.states.component.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react"
+import { fetchUsers } from "../api/api.users.before"
+import { ButtonGeneral, ButtonWrapper, OutsideTable, TableTitleWrapper } from "../styledcomponents/styled.components"
+import { withAsync } from "../helpers/with.async";
+
+/**react-design-patterns-app - version 20.18 - UsersApiStatesHelper
+ * - Features: 
+ * 
+ *     --> Building 'UsersApiStatesHelper' 
+ * 
+ * Note: this component will use a helper function
+*/
+
+const ApiStatus = "IDLE" | "PENDING" | "SUCESS" | "ERROR";
+
+const useFecthUsers = () => {
+    const [ users, setUsers ] = useState([])
+    const [ fetchUsersStatus, setFetchUsersStatus ] = useState("IDLE")
+
+    const initFetchUsers = async () => {
+        setFetchUsersStatus("PENDING");
+        console.log("resulting state => PENDING")
+        const { response, error } = await withAsync(() => fetchUsers())
+
+        if (error) {
+
+            setFetchUsersStatus("ERROR")
+            console.log("resulting state => ERROR")
+
+        }else if (response) {
+
+            setFetchUsersStatus("SUCESS");
+            console.log("resulting state => SUCESS")
+            setUsers(response); 
+            
+        }
+    };
+
+    const CleanUpUsers = () => {
+        return setUsers([])
+    }
+
+    return {
+        users,
+        fetchUsersStatus,
+        initFetchUsers,
+        CleanUpUsers
+    }
+}
+
+
+const UsersApiStatesHelper = () => {
+    const { users, fetchUsersStatus, initFetchUsers, CleanUpUsers } = useFecthUsers();
+
+    useEffect(() => {
+        initFetchUsers();
+    }, []);
+
+    return(
+        <OutsideTable>
+           <ButtonWrapper>
+                <ButtonGeneral>
+                    <button onClick={() => initFetchUsers()}
+                        >{fetchUsersStatus === "PENDING" ? "Loading..." : "Fetch Users"}
+                    </button>    
+                </ButtonGeneral>
+                <ButtonGeneral>
+                    <button onClick={() => CleanUpUsers()}>Clean Up Users</button>    
+                </ButtonGeneral>
+            </ButtonWrapper> 
+        <TableTitleWrapper>
+            <label>name:</label>
+            <label>email:</label>
+        </TableTitleWrapper>
+        {users.map((user) => {
+            const { name, id, email } = user;
+            return( 
+                <ul key={id}>
+                    <li>
+                        {name}  
+                    </li>
+                    <li>
+                        {email} 
+                    </li>
+                 </ul>
+            )
+        })}
+        </OutsideTable>
+    )
+}
+
+export default UsersApiStatesHelper;
