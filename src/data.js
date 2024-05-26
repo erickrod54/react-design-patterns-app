9c669307f1cca6efc210886819b16946f7a1f82c
Erick Rodriguez <erickrod54@gmail.com>
2024-05-25 21:30:00 -0400

    react-design-patterns-app - version 21.05 - data js - Features: Adding UsersApiStatesObject code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 21.04 - data js  
+/**react-design-patterns-app - version 21.05 - data js  
  * - Features: 
  *    
- *     --> Adding 'apiStatus' code
+ *     --> Adding 'UsersApiStatesObject' code
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -4482,6 +4482,91 @@ const toCapital = str => str.charAt(0).toUpperCase() + str.slice(1);
         SUCESS,
         ERROR
     }
+      `
+    },
+    {
+    id: 141,
+    name: 'UsersApiStatesObject - constant object version',
+    code:   
+    `
+    /** the constants will be imported as follows */
+    const useFecthUsers = () => {
+    const [ users, setUsers ] = useState([])
+    const [ fetchUsersStatus, setFetchUsersStatus ] = useState(apiStatus.IDLE)
+  
+      const initFetchUsers = async () => {
+          setFetchUsersStatus(apiStatus.PENDING);
+          console.log("resulting state ==> ", apiStatus.PENDING)
+          const { response, error } = await withAsync(() => fetchUsers())
+  
+          if (error) {
+  
+              setFetchUsersStatus(apiStatus.ERROR)
+              console.log("resulting state => ", apiStatus.ERROR)
+  
+          }else if (response) {
+  
+              setFetchUsersStatus(apiStatus.SUCESS);
+              console.log("resulting state => ", apiStatus.SUCESS)
+              setUsers(response); 
+              
+          }
+      };
+  
+      const CleanUpUsers = () => {
+          return setUsers([])
+      }
+  
+      return {
+          users,
+          fetchUsersStatus,
+          initFetchUsers,
+          CleanUpUsers
+      }
+  }
+  
+  
+  const UsersApiStatesObject = () => {
+        const { users, fetchUsersStatus, initFetchUsers, CleanUpUsers } = useFecthUsers();
+    
+        useEffect(() => {
+            initFetchUsers();
+        }, []);
+    
+        return(
+            <OutsideTable>
+              <ButtonWrapper>
+                    <ButtonGeneral>
+                        <button onClick={() => initFetchUsers()}
+                            >{fetchUsersStatus === apiStatus.PENDING ? "Loading..." : "Fetch Users"}
+                        </button>    
+                    </ButtonGeneral>
+                    <ButtonGeneral>
+                        <button onClick={() => CleanUpUsers()}>Clean Up Users</button>    
+                    </ButtonGeneral>
+                </ButtonWrapper> 
+            <TableTitleWrapper>
+                <label>name:</label>
+                <label>email:</label>
+            </TableTitleWrapper>
+            {users.map((user) => {
+                const { name, id, email } = user;
+                return( 
+                    <ul key={id}>
+                        <li>
+                            {name}  
+                        </li>
+                        <li>
+                            {email} 
+                        </li>
+                    </ul>
+                )
+            })}
+            </OutsideTable>
+        )
+    }
+    
+    export default UsersApiStatesObject;
       `
     }
   ];
