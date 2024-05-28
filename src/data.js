4e0e1ec1537b46b76c61648ddeb888bfb07a2932
Erick Rodriguez <erickrod54@gmail.com>
2024-05-27 21:37:00 -0400

    react-deisgn-patterns-app - version 21.10 - data js - Features: Adding UsersApiStatesHookAndObject code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 21.09 - data js  
+/**react-design-patterns-app - version 21.10 - data js  
  * - Features: 
  *    
- *     --> Adding 'apistatushookAndobjectcode' code comments
+ *     --> Adding 'UsersApiStatesHookAndObject' code 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -4605,6 +4605,107 @@ const toCapital = str => str.charAt(0).toUpperCase() + str.slice(1);
           ...statuses,
       }
   }
+      `
+    },
+    {
+    id: 143,
+    name: 'UsersApiStatesHookAndObject - constant object version',
+    code:   
+    `
+    const useFetchUsers = () => {
+        const [ users, setUsers ] = useState([])
+        
+        const {
+            status: fetchUsersStatus,
+            setStatus: setFetchUsersStatus,
+            isIdle:isFetchUsersStatusIdle,
+            isPending: isFetchUsersStatusPending,
+            isError: isFetchUsersStatusError,
+            isSuccess: isFetchUsersStatusSUCCESS,
+        } = useApiStatus(apiStatus.IDLE);
+    
+        const initFetchUsers = async () => {
+            setFetchUsersStatus(apiStatus.PENDING);
+            console.log("resulting state ==> ", apiStatus.PENDING)
+            const { response, error } = await withAsync(() => fetchUsers())
+    
+            if (error) {
+    
+                setFetchUsersStatus(apiStatus.ERROR)
+                console.log("resulting state => ", apiStatus.ERROR)
+    
+            }else if (response) {
+    
+                setFetchUsersStatus(apiStatus.SUCESS);
+                console.log("resulting state => ", apiStatus.SUCESS)
+                setUsers(response); 
+                
+            }
+        };
+    
+        const CleanUpUsers = () => {
+            return setUsers([])
+        }
+    
+        return {
+            users,
+            isFetchUsersStatusIdle,
+            isFetchUsersStatusPending,
+            isFetchUsersStatusError,
+            isFetchUsersStatusSUCCESS,
+            initFetchUsers,
+            CleanUpUsers
+        }
+    }
+    
+    
+    const UsersApiStatesHookAndObject = () => {
+        const { users,
+                initFetchUsers,
+                isFetchUsersStatusError,
+                isFetchUsersStatusIdle,
+                isFetchUsersStatusPending,
+                isFetchUsersStatusSUCCESS,
+                CleanUpUsers } = useFetchUsers();
+    
+        useEffect(() => {
+            initFetchUsers();
+        }, []);
+    
+        return(
+            <OutsideTable>
+              <ButtonWrapper>
+                    <ButtonGeneral>
+                        <button onClick={() => initFetchUsers()}
+                            >{isFetchUsersStatusIdle ? "Loading..." : "Fetch Users"}
+                        </button>    
+                    </ButtonGeneral>
+                    <ButtonGeneral>
+                        <button onClick={() => CleanUpUsers()}>Clean Up Users</button>    
+                    </ButtonGeneral>
+                </ButtonWrapper> 
+            <TableTitleWrapper>
+                <label>name:</label>
+                <label>email:</label>
+            </TableTitleWrapper>
+            {users.map((user) => {
+                const { name, id, email } = user;
+                return( 
+                    <ul key={id}>
+                        <li>
+                            {name}  
+                        </li>
+                        <li>
+                            {email} 
+                        </li>
+                    </ul>
+                )
+            })}
+            </OutsideTable>
+        )
+    }
+    
+     export default UsersApiStatesHookAndObject;
       `
     }
   ];
