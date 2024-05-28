8373e0c02ac85762606386a4695f26c719c43960
Erick Rodriguez <erickrod54@gmail.com>
2024-05-27 21:34:00 -0400

    react-design-patterns-app - version 21.07 - index > hooks - Features: importing and exporting useApiStatus

---
+++ src/hooks/index.hooks.js
@@ -0,0 +1,11 @@
+import { useApiStatus } from "./api.status.hook";
+
+/**react-design-patterns-app - version 21.07 - index > hooks
+ * - Features: 
+ * 
+ *     --> importing and exporting 'useApiStatus' 
+ * 
+ * Note: this component will use a helper function
+*/
+
+export { useApiStatus }
