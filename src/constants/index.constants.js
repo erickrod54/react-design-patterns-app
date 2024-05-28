842793fe0bc9cf0a88533ce737f41c4d36ff46c7
Erick Rodriguez <erickrod54@gmail.com>
2024-05-27 21:34:00 -0400

    react-design-patterns-app - version 21.07 - index > constants - Features: Importing and exporting defaultApiStatuses

---
+++ src/constants/index.constants.js
@@ -0,0 +1,11 @@
+import { defaultApiStatuses } from "./api.status.object";
+
+/**react-design-patterns-app - version 21.07 - index > constants
+ * - Features: 
+ * 
+ *     --> importing and exporting 'defaultApiStatuses' 
+ * 
+ * Note: this component will use a helper function
+*/
+
+export { defaultApiStatuses }
