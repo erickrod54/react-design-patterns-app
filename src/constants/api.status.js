c436cb7878e46dccb8e837bc5047b8f0e4ae1f74
Erick Rodriguez <erickrod54@gmail.com>
2024-05-30 21:35:00 -0400

    react-design-patterns-app - version 21.19 - api.status - Features: Fixing SUCCESS typo in main file

--- src/constants/api.status.js
+++ src/constants/api.status.js
@@ -1,4 +1,13 @@
+/**react-design-patterns-app - version 21.19 - api.status
+ * - Features: 
+ * 
+ *     --> Fixing 'SUCCESS' typo in main file
+ * 
+ * Note: 'UserInfo' with a few modifications ends up in 
+ * 'UserInfoWithHook'  
+*/
+
 export const IDLE = "IDLE";
 export const PENDING = "PENDING";
-export const SUCESS = "SUCESS";
+export const SUCCESS = "SUCCESS";
 export const ERROR = "ERROR";
