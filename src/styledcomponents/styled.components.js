f2650dcc6e3f30bb28dcc4dc1908106d2b634841
Erick Rodriguez <erickrod54@gmail.com>
2024-05-31 21:31:00 -0400

    react-design-patterns-app - version 21.19 - styled.components - Features: writting api-abstraction-logic styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 20.14 - styled.components
+/**react-design-patterns-app - version 21.19 - styled.components
  * - Features: 
  * 
- *     --> writting 'api-states' styles.	
+ *     --> writting 'api-abstraction-logic' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -502,6 +502,12 @@ export const CustomTitleStyle = styled.div`
 .api-states{
 	span{
 			${tw`text-indigo-300 underline`}
+		}
+}
+
+.api-abstraction-logic{
+	span{
+			${tw`text-red-300 underline`}
 		}
 }
 
