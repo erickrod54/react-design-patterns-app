6dc48930f89b1ad15b585c4182d7e5b27f505be0
Erick Rodriguez <erickrod54@gmail.com>
2024-06-01 21:30:00 -0400

    react-design-patterns-app - version 22.00 - data js - Features: Adding UsersApiHookLogicAndDataAbs code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 21.20 - data js  
+/**react-design-patterns-app - version 22.00 - data js  
  * - Features: 
  *    
- *     --> Adding 'useApi for data and logic abstraction' id
+ *     --> Adding 'UsersApiHookLogicAndDataAbs' code
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -4897,6 +4897,83 @@ const UsersApiLazyLoaderHookAndObject = () => {
             ...normalizedStatuses,
         }
     }
+    `
+    },
+    {
+    id: 147,
+    name: 'UsersApiHookLogicAndDataAbs',
+    code:   
+    `
+    const useFetchUsers = () => {
+
+    const {
+        data: users,
+        exec: initFetchUsers,
+        status: fetchUsersStatus,
+        isIdle: isFetchUsersStatusIdle,
+        isPending: isFetchUsersStatusPending,
+        isError: isFetchUsersStatusError,
+        isSuccess: isFetchUsersStatusSuccess,
+      } = useApi(() => fetchUsers().then((response) => response.data));
+
+    return {
+        users,
+        isFetchUsersStatusIdle,
+        isFetchUsersStatusPending,
+        isFetchUsersStatusError,
+        isFetchUsersStatusSuccess,
+        initFetchUsers
+    }
+}
+
+
+const UsersApiHookLogicAndDataAbs = () => {
+
+    const {
+        users,
+        initFetchUsers,
+        isFetchUsersStatusError,
+        isFetchUsersStatusIdle,
+        isFetchUsersStatusPending,
+        isFetchUsersStatusSuccess } = useFetchUsers();
+
+        useEffect(() => {
+            initFetchUsers();
+            console.log("resulting users ==> ", users)
+        }, []);
+    
+        return(
+            <OutsideTable>
+           <ButtonWrapper>
+                <ButtonGeneral>
+                    <button onClick={() => initFetchUsers() && console.log("fetching users...")}
+                        > 
+                        {/**instead of the condition the LazyLoader with the props to data flow control */}
+                        <LazyLoader show={isFetchUsersStatusPending} delay={500} default="Fetch Users"/>
+                    </button>    
+                </ButtonGeneral>
+            </ButtonWrapper> 
+        <TableTitleWrapper>
+            <label>name:</label>
+            <label>email:</label>
+        </TableTitleWrapper>
+        {
+            users && users.map((user) => {
+                const { name, id, email } = user;
+                
+                return (
+                    <ul key={id}>
+                        <li>{name}</li>
+                        <li>{email}</li>
+                    </ul>
+                )
+            })
+        }
+        </OutsideTable>
+        )
+    }
+
+    export default UsersApiHookLogicAndDataAbs;
     `
     }
   ];
