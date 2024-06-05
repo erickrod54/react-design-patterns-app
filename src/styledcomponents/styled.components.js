bdce5082d7c069cb9cfc0e83edfa4cfa1a19b847
Erick Rodriguez <erickrod54@gmail.com>
2024-06-04 21:35:00 -0400

    react-deisng-patterns-app - version 22.05 - styled.components - Features: writting Fieldset styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 22.04 - styled.components
+/**react-design-patterns-app - version 22.05 - styled.components
  * - Features: 
  * 
- *     --> writting 'Form' styles.	
+ *     --> writting 'Fieldset' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -28,6 +28,11 @@ export const MainMealContainer = styled.div`
 
 export const Form = styled.form`
   margin-bottom: 9px;
+`;
+
+export const Fieldset = styled.fieldset`
+  display: flex;
+  flex-direction: column;
 `;
 
 export const ShoppingCounterWrapper = styled.div`
