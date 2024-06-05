5fccd09545c9ed93b9e4745a2d48f3a5b6f66bf0
Erick Rodriguez <erickrod54@gmail.com>
2024-06-04 21:34:00 -0400

    react-design-patterns-app - version 22.04 - styled.components - Features: writting Form styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 22.03 - styled.components
+/**react-design-patterns-app - version 22.04 - styled.components
  * - Features: 
  * 
- *     --> writting 'MainMealContainer' styles.	
+ *     --> writting 'Form' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -24,6 +24,10 @@ export const MainMealContainer = styled.div`
   background-color: yellow;
   color: black;
   
+`;
+
+export const Form = styled.form`
+  margin-bottom: 9px;
 `;
 
 export const ShoppingCounterWrapper = styled.div`
