f2ce848dfe723161aa9811985d0e3153091901cb
Erick Rodriguez <erickrod54@gmail.com>
2024-06-05 21:36:00 -0400

    react-design-patterns-app - version 22.07 - styled.components - Features: writting Input styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 22.06 - styled.components
+/**react-design-patterns-app - version 22.07 - styled.components
  * - Features: 
  * 
- *     --> writting 'MealLabel' styles.	
+ *     --> writting 'Input' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -38,6 +38,12 @@ export const Fieldset = styled.fieldset`
 export const MealLabel = styled.label`
   margin-bottom: 4px;
   font-weight: bold;
+`;
+
+export const Input = styled.input`
+  padding: 4px;
+  border: 1px solid orange;
+  border-radius: 8px;
 `;
 
 export const ShoppingCounterWrapper = styled.div`
