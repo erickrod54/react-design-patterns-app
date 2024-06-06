eb1afc9514aef5483e1ecc5a1b0b0bb9265646e2
Erick Rodriguez <erickrod54@gmail.com>
2024-06-05 21:33:00 -0400

    react-design-patterns-app - version 22.06 - styled.components - Features: writting MealLabel styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 22.05 - styled.components
+/**react-design-patterns-app - version 22.06 - styled.components
  * - Features: 
  * 
- *     --> writting 'Fieldset' styles.	
+ *     --> writting 'MealLabel' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -33,6 +33,11 @@ export const Form = styled.form`
 export const Fieldset = styled.fieldset`
   display: flex;
   flex-direction: column;
+`;
+
+export const MealLabel = styled.label`
+  margin-bottom: 4px;
+  font-weight: bold;
 `;
 
 export const ShoppingCounterWrapper = styled.div`
