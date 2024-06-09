7d26ddaad17d11c0e32716380f14ead598786750
Erick Rodriguez <erickrod54@gmail.com>
2024-06-08 21:38:00 -0400

    react-design-patterns-app - version 22.12 - data js - Features: Adding API layer - SearchMeals - axios cancellation code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 22.01 - data js  
+/**react-design-patterns-app - version 22.12 - data js  
  * - Features: 
  *    
- *     --> Adding 'Use Case Meal Search API' 
+ *     --> Adding 'API layer - SearchMeals - axios cancellation' 
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -4981,9 +4981,75 @@ const UsersApiHookLogicAndDataAbs = () => {
 
     export default UsersApiHookLogicAndDataAbs;
     `
+    },
+    {
+    id: 147,
+    name: 'API layer - SearchMeals - axios cancellation',
+    code:   
+    `
+    const axiosParams = {
+    // Set different base URL based on the environment
+    baseURL:
+        process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/",
+    };
+    
+    // Create axios instance with default params
+    const axiosInstance = axios.create(axiosParams);
+    
+    export const didAbort = (error) => axios.isCancel(error) && { aborted: true };
+    
+    const getCancelSource = () => axios.CancelToken.source();
+    
+    export const isApiError = (error) => axios.isAxiosError(error);
+    
+    const withAbort = (fn) => {
+      const executor = async (...args) => {
+        const originalConfig = args[args.length - 1];
+        const { abort, ...config } = originalConfig;
+    
+        if (typeof abort === "function") {
+          const { cancel, token } = getCancelSource();
+          config.cancelToken = token;
+          abort(cancel);
+        }
+    
+        try {
+          if (args.length > 2) {
+            const [url, body] = args;
+            return await fn(url, body, config);
+          } else {
+            const [url] = args;
+            return await fn(url, config);
+          }
+        } catch (error) {
+          console.log("api error", error);
+    
+          if (didAbort(error)) {
+            error.aborted = true;
+          }
+    
+          throw error;
+        }
+      };
+    
+      return executor;
+    };
+    
+    const api = (axios) => {
+      return {
+        get: (url, config = {}) => withAbort(axios.get)(url, config),
+        delete: (url, config = {}) => withAbort(axios.delete)(url, config),
+        post: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
+        patch: (url, body, config = {}) =>
+          withAbort(axios.patch)(url, body, config),
+        put: (url, body, config = {}) => withAbort(axios.put)(url, body, config),
+      };
+    };
+    
+    export default api(axiosInstance);
+    `
     }
   ];
-
 
   /**List Pattern data  -- start */
   
