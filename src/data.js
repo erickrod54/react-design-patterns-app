8cba6686a25e19000aa20f0dfc513ef622b37fa3
Erick Rodriguez <erickrod54@gmail.com>
2024-06-11 21:33:00 -0400

    react-design-patterns-app - version 23.03 - data js - Adding withLogger to API layer Meal Search code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 23.01 - data js  
+/**react-design-patterns-app - version 23.03 - data js  
  * - Features: 
  *    
- *     --> Adding SearchMealComponent comments 
+ *     --> Adding withLogger to API layer Meal Search code
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -5171,6 +5171,91 @@ const UsersApiHookLogicAndDataAbs = () => {
     };
     
     export default SearchMealComponent;
+    `
+    },
+    {
+    id: 150,
+    name: 'Adding withLogger to API layer Meal Search',
+    code:   
+    `
+        const axiosParams = {
+      // Set different base URL based on the environment
+      baseURL:
+        process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/",
+    };
+    
+    // Create axios instance with default params
+    const axiosInstance = axios.create(axiosParams);
+    
+    export const didAbort = (error) => axios.isCancel(error) && { aborted: true };
+    
+    const getCancelSource = () => axios.CancelToken.source();
+    
+    export const isApiError = (error) => axios.isAxiosError(error);
+    
+    const withAbort = (fn) => {
+      const executor = async (...args) => {
+        const originalConfig = args[args.length - 1];
+        const { abort, ...config } = originalConfig;
+    
+        if (typeof abort === "function") {
+          const { cancel, token } = getCancelSource();
+          config.cancelToken = token;
+          abort(cancel);
+        }
+    
+        try {
+          if (args.length > 2) {
+            const [url, body] = args;
+            return await fn(url, body, config);
+          } else {
+            const [url] = args;
+            return await fn(url, config);
+          }
+        } catch (error) {
+          console.log("api error", error);
+    
+          if (didAbort(error)) {
+            error.aborted = true;
+          }
+    
+          throw error;
+        }
+      };
+    
+      return executor;
+    };
+  
+    const withLogger = async (promise) => 
+        promise.catch((error) => {
+            if (!process.env.REACT_APP_DEBUG_API) throw error;
+    
+            if (error.response) {
+                console.log(error.response.data)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+            } else if(error.request){
+                console.log(error.request)
+            }else{
+                console.log("An error has ocurred",error.message)
+            }
+            console.log(error.config)
+            throw error;
+        })
+  
+  
+    const api = (axios) => {
+      return {
+        get: (url, config = {}) => withLogger(withAbort(axios.get)(url, config)),
+        delete: (url, config = {}) => withLogger(withAbort(axios.delete)(url, config)),
+        post: (url, body, config = {}) => withLogger(withAbort(axios.post)(url, body, config)),
+        patch: (url, body, config = {}) =>
+          withLogger(withAbort(axios.patch)(url, body, config)),
+        put: (url, body, config = {}) => withLogger(withAbort(axios.put)(url, body, config)),
+      };
+    };
+    
+    export default api(axiosInstance);
     `
     }
   ];
