29423d83bbcd102dbbe30f29cb9ab917e301f8ff
Erick Rodriguez <erickrod54@gmail.com>
2024-06-11 21:34:00 -0400

    react-design-patterns-app - version 23.03 - Api Layer Index - Features: Importing and exporting searchMealsErrorLogin

---
+++ src/api/api.layer.index.js
@@ -0,0 +1,11 @@
+import { searchMealsErrorLogin } from './api.layer.use.case.meal.search.error.loging';
+
+/**react-design-patterns-app - version 23.03 - Api Layer index
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'searchMealsErrorLogin'
+ * 
+ * Note: pending to import and export the rest of the API's 
+ */
+
+export { searchMealsErrorLogin }
