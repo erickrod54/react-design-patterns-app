79f305e0e2c8d0d7700743d8eac2a49250aa17fe
Erick Rodriguez <erickrod54@gmail.com>
2024-06-16 21:31:00 -0400

    react-design-patterns-app - version 22.13 - styled.components - Features: writting fetching-react-query styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 22.12 - styled.components
+/**react-design-patterns-app - version 22.13 - styled.components
  * - Features: 
  * 
- *     --> updating writting 'SearchMealComponent' styles.	
+ *     -->  writting 'fetching-data-withj-react-query' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -552,6 +552,12 @@ export const CustomTitleStyle = styled.div`
 .use-cases-meal-search{
 	span{
 			${tw`text-amber-100 underline`}
+		}	
+}
+
+.fetching-data-withj-react-query{
+	span{
+			${tw`text-amber-200 underline`}
 		}	
 }
 
