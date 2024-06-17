8cef1a6442efbd441d5581e48b3a2ee75524ba26
Erick Rodriguez <erickrod54@gmail.com>
2024-06-16 21:35:00 -0400

    react-design-patterns-app - version 23.14 - FetchingDataWithReactQuery - Features: Starting Fetching Data with ReactQuery

--- src/components/fetching.data.with.data.query.jsx
+++ src/components/fetching.data.with.data.query.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import {  CustomTitleStyle, SectionWrapper } from "../styledcomponents/styled.components";
 import CustomTitle from "./custom.title.component";
 import { usePatternsAppContext } from "../context";
+import FetchTopQuotes from "./api.layer.use.cases.quotes.component";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 
-/**react-design-patterns-app - version 23.13 - FetchingDataWitReactQuery
+/**react-design-patterns-app - version 23.14 - FetchingDataWitReactQuery
  * - Features: 
  * 
- *     --> Building 'Fetching Data with ReactQuery'  
+ *     --> Starting 'Fetching Data with ReactQuery'  
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
 */
+
+const queryClient = new QueryClient();
 
 const FetchingDataWitReactQuery = () => {
 
@@ -24,7 +28,12 @@ const FetchingDataWitReactQuery = () => {
             </CustomTitleStyle>
 
             <SectionWrapper>
-
+                
+                <p>
+                    In this section is going to be covered how <span className="text-white font-semibold"> manage API calls effectively </span>
+                    and for this implementation is going to be used <span className="text-white font-semibold"> QueryClientProvider </span>
+                    that belongs to the library <span className="text-white font-semibold"> react-query </span>
+                </p>
             </SectionWrapper>
         
         </div>
