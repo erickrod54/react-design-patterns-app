997987c433ebea7a22f37b213629c7a4adfc33b3
Erick Rodriguez <erickrod54@gmail.com>
2024-06-17 21:30:00 -0400

    react-design-patterns-app - version 23.15 - FetchingDataWithReactQuery - Features: Starting server.quotes.js

--- src/components/fetching.data.with.data.query.jsx
+++ src/components/fetching.data.with.data.query.jsx
@@ -6,10 +6,10 @@ import FetchTopQuotes from "./api.layer.use.cases.quotes.component";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 
-/**react-design-patterns-app - version 23.14 - FetchingDataWitReactQuery
+/**react-design-patterns-app - version 23.15 - FetchingDataWitReactQuery
  * - Features: 
  * 
- *     --> Starting 'Fetching Data with ReactQuery'  
+ *     --> Starting 'server.quotes.js'  
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
@@ -33,6 +33,11 @@ const FetchingDataWitReactQuery = () => {
                     In this section is going to be covered how <span className="text-white font-semibold"> manage API calls effectively </span>
                     and for this implementation is going to be used <span className="text-white font-semibold"> QueryClientProvider </span>
                     that belongs to the library <span className="text-white font-semibold"> react-query </span>
+                </p>
+
+                <p>
+                    First the data that will be consume by this app will come from an express js server 
+                    <span className="text-white font-semibold"> server.quotes.js </span> as follows:
                 </p>
             </SectionWrapper>
         
