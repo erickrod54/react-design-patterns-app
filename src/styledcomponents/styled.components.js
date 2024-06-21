425af31d2d4db85bda9af2cd166f54337dd52d60
Erick Rodriguez <erickrod54@gmail.com>
2024-06-20 21:34:00 -0400

    react-design-patterns-app -version 24.06 - styled.components - Features: writting LoadingMessage styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 24.05 - styled.components
+/**react-design-patterns-app - version 24.06 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ErrorMessage' styles.	
+ *     -->  writting 'LoadingMessage' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,8 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const LoadingMessage = styled.p``;
 
 export const ErrorMessage = styled.p`
   color: #e53e3e; /* Tailwind: text-red-900 */
