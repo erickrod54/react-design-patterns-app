186be7c9151a386c3f0d589b5a4e505095fbcc77
Erick Rodriguez <erickrod54@gmail.com>
2024-06-21 21:30:00 -0400

    react-design-patterns-app - version 24.07 - styled.components - Features: writting QuotesContainer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 24.06 - styled.components
+/**react-design-patterns-app - version 24.07 - styled.components
  * - Features: 
  * 
- *     -->  writting 'LoadingMessage' styles.	
+ *     -->  writting 'QuotesContainer' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const QuotesContainer = styled.div`
+	max-height: 96;
+	overflow-y: auto;
+	border-spacing: 1px;
+`;
 
 export const LoadingMessage = styled.p``;
 
