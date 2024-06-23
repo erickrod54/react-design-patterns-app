236f662e7eaefde776882a9850ef192b307e6301
Erick Rodriguez <erickrod54@gmail.com>
2024-06-22 21:33:00 -0400

    react-design-patterns-app - version 24.10 - styled.components - Features: writting AuthorCite styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 24.09 - styled.components
+/**react-design-patterns-app - version 24.10 - styled.components
  * - Features: 
  * 
- *     -->  writting 'QuoteText' styles.	
+ *     -->  writting 'AuthorCite' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const AuthorCite = styled.cite`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 export const QuoteText = styled.p`
 	margin-bottom: 4px;
