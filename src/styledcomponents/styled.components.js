5ede42a6b70ca4780b1dc23f16aea98a46b655a3
Erick Rodriguez <erickrod54@gmail.com>
2024-06-23 21:33:00 -0400

    react-design-patterns-app - version 24.12 - styled.components - Features: writting AuthorName styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 24.11 - styled.components
+/**react-design-patterns-app - version 24.12 - styled.components
  * - Features: 
  * 
- *     -->  writting 'AuthorInfo' styles.	
+ *     -->  writting 'AuthorName' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const AuthorName = styled.span`
+	margin-bottom: 1px;
+	font-size: sm;
+	font-style: italic;
+	font-weight: bold;
+`;
 
 export const AuthorInfo = styled.div`
 	display: flex;
