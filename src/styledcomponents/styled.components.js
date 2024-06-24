9f5923423a75b348fa177bfdae193b958d2a5c53
Erick Rodriguez <erickrod54@gmail.com>
2024-06-23 21:30:00 -0400

    react-design-patterns-app - version 24.11 - styled.components - Features: writting AuthorInfo

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 24.10 - styled.components
+/**react-design-patterns-app - version 24.11 - styled.components
  * - Features: 
  * 
- *     -->  writting 'AuthorCite' styles.	
+ *     -->  writting 'AuthorInfo' styles.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const AuthorInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+`;
 
 export const AuthorCite = styled.cite`
 	display: flex;
