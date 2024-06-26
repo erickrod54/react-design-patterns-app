4f8c08e0d07fedca1195217313f15ec9f45c2747
Erick Rodriguez <erickrod54@gmail.com>
2024-06-25 21:31:00 -0400

    react-design-patterns-app - version 24.20 - styled.components - Features: writting AuthorInfo styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 24.19 - styled.components
+/**react-design-patterns-app - version 24.20 - styled.components
  * - Features: 
  * 
- *     -->  writting 'AuthorCite' styles to tailwind.	
+ *     -->  writting 'AuthorInfo' styles to tailwind.	
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -25,9 +25,8 @@ export const AuthorName = styled.span`
 `;
 
 export const AuthorInfo = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
+	${tw`flex flex-col items-start`}
+
 `;
 
 export const AuthorCite = styled.cite`
