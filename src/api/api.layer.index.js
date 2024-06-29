b67487de1e0b15a369f42f9e71a83c487f3d8e26
Erick Rodriguez <erickrod54@gmail.com>
2024-06-28 21:34:00 -0400

    react-design-patterns-app - version 25.11 - Api Layer index - Features: Importing and exporting postQuote and resetQuotes endpoints

--- src/api/api.layer.index.js
+++ src/api/api.layer.index.js
@@ -1,12 +1,13 @@
 import { fetchTopQuotes } from './api.layer.quotes.request';
+import { postQuote, resetQuotes } from './api.layer.use.case.quotes.post.update';
 
 
-/**react-design-patterns-app - version 23.14 - Api Layer index
+/**react-design-patterns-app - version 25.11 - Api Layer index
  * - Features: 
  * 
- *     --> Importing and exporting 'fetchTopQuotes'
+ *     --> Importing and exporting 'postQuote, resetQuotes '
  * 
  * Note: pending to import and export the rest of the API's 
  */
 
-export {  fetchTopQuotes }
+export {  fetchTopQuotes, postQuote, resetQuotes }
