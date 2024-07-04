d1a9fb2bc4f19c87a4734c7f3ab2e31f22c818f5
Erick Rodriguez <erickrod54@gmail.com>
2024-07-03 21:31:00 -0400

    react-design-patterns-app - version 26.02 - styled.components - Features: writting FormPost styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 26.01 - styled.components
+/**react-design-patterns-app - version 26.02 - styled.components
  * - Features: 
  * 
- *     -->  writting 'TitlePostForm' styles 
+ *     -->  writting 'FormPost' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,15 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const FormPost = styled.form`
+	display: flex;
+	flex-direction: column;
+	gap: 24px;
+	max-width: lg;
+	margin: auto;
+	text-align: left;
+`;
 
 export const TitlePostForm = styled.h2`
   font-weight: bold;
