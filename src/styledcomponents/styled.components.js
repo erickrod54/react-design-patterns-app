7c51c368f0d1960e6564a64083dc5a022ee3070f
Erick Rodriguez <erickrod54@gmail.com>
2024-07-03 21:34:00 -0400

    react-design-patterns-app - version 26.03 - styled.components - Features: writting FormGroup styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 26.02 - styled.components
+/**react-design-patterns-app - version 26.03 - styled.components
  * - Features: 
  * 
- *     -->  writting 'FormPost' styles 
+ *     -->  writting 'FormGroup' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -17,6 +17,11 @@ import tw from 'twin.macro';
  * to use them in the custom titles
  */
 
+export const FormGroup = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+`;
 export const FormPost = styled.form`
 	display: flex;
 	flex-direction: column;
