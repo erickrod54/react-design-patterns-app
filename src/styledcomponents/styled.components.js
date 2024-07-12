f27787d0933473b0c405b19595820a2f1cb35a01
Erick Rodriguez <erickrod54@gmail.com>
2024-07-11 21:31:00 -0400

    react-design-patterns-app - version 27.09 - styled.components - Features: writting TitlePaginated styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 27.08 - styled.components
+/**react-design-patterns-app - version 27.09 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ContainerPaginated' styles 
+ *     -->  writting 'TitlePaginated' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,18 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const TitlePaginated = styled.h2`
+	font-weight: bold;
+	font-size: 2rem;
+	margin-bottom: 1rem;
+	color: darkorange;
+	background-color: lavender;
+	width: fit-content;
+	border-radius: 1rem;
+	padding: 8px;
+	margin: 8px;
+`;
 
 export const ContainerPaginated = styled.div`
   padding: 8px;
