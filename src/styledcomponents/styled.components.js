cf069c0ee0b89259da49940c1d219f74f4427dc8
Erick Rodriguez <erickrod54@gmail.com>
2024-07-11 21:34:00 -0400

    react-design-patterns-app - version 27.10 - styled.components - Features: writting ErrorMessagePagination styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 27.09 - styled.components
+/**react-design-patterns-app - version 27.10 - styled.components
  * - Features: 
  * 
- *     -->  writting 'TitlePaginated' styles 
+ *     -->  writting 'ErrorMessagePagination' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -17,6 +17,9 @@ import tw from 'twin.macro';
  * to use them in the custom titles
  */
 
+export const ErrorMessagePagination = styled.p`
+	color: #c53030;
+`;
 export const TitlePaginated = styled.h2`
 	font-weight: bold;
 	font-size: 2rem;
