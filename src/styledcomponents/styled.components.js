1969025053804367ed317bc1577eb8cd770aef46
Erick Rodriguez <erickrod54@gmail.com>
2024-07-14 21:31:00 -0400

    react-design-patterns-app - version 27.15 - styled.components - Features: writting CiteContainer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 27.14 - styled.components
+/**react-design-patterns-app - version 27.15 - styled.components
  * - Features: 
  * 
- *     -->  writting 'QuoteTextPagination' styles 
+ *     -->  writting 'CiteContainer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const CiteContainer = styled.cite`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 export const QuoteTextPagination = styled.p`
 	margin-bottom: 1rem;
