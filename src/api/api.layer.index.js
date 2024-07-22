16043e22098af71fd079d1fada420e25bce3daba
Erick Rodriguez <erickrod54@gmail.com>
2024-07-21 21:35:00 -0400

    react-design-patterns-app - version 28.18 - Api Layer index - Features: Importing and exporting fetchQuotesByCursor

--- src/api/api.layer.index.js
+++ src/api/api.layer.index.js
@@ -1,14 +1,15 @@
 import { fetchTopQuotes } from './api.layer.quotes.request';
+import { fetchQuotesByCursor } from './api.layer.use.case.quotes.infinite.scroll';
 import { fetchQuotesByPage } from './api.layer.use.case.quotes.pagination';
 import { postQuote, resetQuotes } from './api.layer.use.case.quotes.post.update';
 
 
-/**react-design-patterns-app - version 27.05 - Api Layer index
+/**react-design-patterns-app - version 28.18 - Api Layer index
  * - Features: 
  * 
- *     --> Importing and exporting 'fetchQuotesByPage'
+ *     --> Importing and exporting 'fetchQuotesByCursor'
  * 
  * Note: pending to import and export the rest of the API's 
  */
 
-export {  fetchTopQuotes, postQuote, resetQuotes, fetchQuotesByPage }
+export {  fetchTopQuotes, postQuote, resetQuotes, fetchQuotesByPage, fetchQuotesByCursor }
