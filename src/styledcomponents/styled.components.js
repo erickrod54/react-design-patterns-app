f5bb1e0098a356e0e17d1f610e444ffbb315ed01
Erick Rodriguez <erickrod54@gmail.com>
2024-07-22 21:34:00 -0400

    react-design-patterns-app - version 28.20 - styled.components - Features: writting ContainerScroll styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 28.19 - styled.components
+/**react-design-patterns-app - version 28.20 - styled.components
  * - Features: 
  * 
- *     -->  writting 'fetching-data-withj-react-query-infinite-scroll-quotes' styles  to tailwind
+ *     -->  writting 'ContainerScroll' styles  to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const ContainerScroll = styled.div`
+  padding: 2rem 0;
+  max-width: 2xl;
+  margin: auto;
+`;
 
 export const LoadingSpan = styled.span`
 	${tw`pl-6 text-orange-500`}
