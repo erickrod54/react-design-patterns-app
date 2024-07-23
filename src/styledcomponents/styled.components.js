5f4b8bdaff52e50a5a06c302338f6fd050a96d9b
Erick Rodriguez <erickrod54@gmail.com>
2024-07-22 21:31:00 -0400

    react-design-patterns-app - version 28.19 - styled.components - Features: writting fetching-adata-with-react-query-inifinite-scroll-quotes styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 28.09 - styled.components
+/**react-design-patterns-app - version 28.19 - styled.components
  * - Features: 
  * 
- *     -->  writting 'LoadingSpan' styles  to tailwind
+ *     -->  writting 'fetching-data-withj-react-query-infinite-scroll-quotes' styles  to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -718,6 +718,12 @@ export const CustomTitleStyle = styled.div`
 .fetching-data-withj-react-query-paginated-quotes{
 	span{
 			${tw`text-amber-500 underline`}
+		}	
+}
+
+.fetching-data-withj-react-query-infinite-scroll-quotes{
+	span{
+			${tw`text-amber-300 underline`}
 		}	
 }
 
