2a1be536dd5d9d680654c92c763b86a37ee9d0d8
Erick Rodriguez <erickrod54@gmail.com>
2024-07-23 21:34:00 -0400

    react-design-patterns-app - version 29.02 - styled.components - Features: writting ErrorMessageScroll styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 29.00 - styled.components
+/**react-design-patterns-app - version 29.02 - styled.components
  * - Features: 
  * 
- *     -->  writting 'TitleScroll' styles  to tailwind
+ *     -->  writting 'ErrorMessageScroll' styles  to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,10 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const ErrorMessageScroll = styled.p`
+color: #c53030;
+`;
 
 export const TitleScroll = styled.h2`
 	font-weight: bold;
