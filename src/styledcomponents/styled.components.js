56bb98ac1522c4a59743e223151e73bca7d69a0c
Erick Rodriguez <erickrod54@gmail.com>
2024-07-24 21:34:00 -0400

    react-design-patterns-app - version 29.04 - styled.components - Features: writting QuotesContainerScroll styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 29.03 - styled.components
+/**react-design-patterns-app - version 29.04 - styled.components
  * - Features: 
  * 
- *     -->  writting 'LoadingMessageScroll' styles  to tailwind
+ *     -->  writting 'QuotesContainerScroll' styles
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,8 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const QuotesContainerScroll = styled.div``;
 
 export const LoadingMessageScroll = styled.p``;
 
