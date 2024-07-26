e4688f2f0b910bb804c3c70a55f89b52538e7a12
Erick Rodriguez <erickrod54@gmail.com>
2024-07-25 21:31:00 -0400

    react-design-patterns-app - version 29.05 - styled.components - Features: writting QuoteBlockScroll styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 29.04 - styled.components
+/**react-design-patterns-app - version 29.05 - styled.components
  * - Features: 
  * 
- *     -->  writting 'QuotesContainerScroll' styles
+ *     -->  writting 'QuoteBlockScroll' styles
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,17 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const QuoteBlockScroll = styled.blockquote`
+  position: relative;
+  padding: 1rem;
+  font-size: 1.5rem;
+  font-style: italic;
+  border-left: 4px solid #718096;
+  background-color: #f7fafc;
+  color: #4a5568;
+  margin-bottom: 1rem;
+`;
 
 export const QuotesContainerScroll = styled.div``;
 
