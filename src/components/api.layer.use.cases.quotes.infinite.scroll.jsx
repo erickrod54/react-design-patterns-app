1dff2c0db307029b8ef3d65bdf640f73ca33d3e7
Erick Rodriguez <erickrod54@gmail.com>
2024-07-29 21:30:00 -0400

    react-design-patterns-app - version 29.11 - InfiniteScrollQuotes - Features: Migrating HiddenMessage style component

--- src/components/api.layer.use.cases.quotes.infinite.scroll.jsx
+++ src/components/api.layer.use.cases.quotes.infinite.scroll.jsx
@@ -8,34 +8,24 @@ import { AuthorTextScroll,
          CiteContainerScroll,
          ContainerScroll,
          ErrorMessageScroll,
+         HiddenMessage,
          LoadingMessageScroll,
-         QuoteBlockScroll, QuotesContainerScroll, QuoteTextScroll, TitleScroll, 
+         QuoteBlockScroll,
+         QuotesContainerScroll,
+         QuoteTextScroll,
+         TitleScroll,
          ToggleTextScroll} from "../styledcomponents/styled.components";
 
-/**react-design-patterns-app - version 29.10 - InfiniteScrollQuotes
+/**react-design-patterns-app - version 29.11 - InfiniteScrollQuotes
  * - Features: 
  * 
- *     --> Migrating 'ToggleTextScroll' style component
+ *     --> Migrating 'HiddenMessage' style component
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
 */
 
 
-
-
-
-const HiddenMessage = styled.h2`
-  font-size: 1.5rem; /* Large, readable size */
-  color: #718096; /* Neutral gray for a subtle message */
-  text-align: center; /* Center the text */
-  margin-top: 1rem; /* Add space above the message */
-  background-color: #f7fafc; /* Light background for emphasis */
-  padding: 0.5rem 1rem; /* Add padding for better readability */
-  border-radius: 0.5rem; /* Rounded corners */
-  border: 1px solid #e2e8f0; /* Light border for definition */
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
-`;
 
 const InfiniteScrollQuotes = () => {
   const { ref: loadMoreRef, inView } = useInView();
