ac6670880e0bd459ea3f61abe99631f2e03e8302
Erick Rodriguez <erickrod54@gmail.com>
2024-07-29 21:35:00 -0400

    react-design-patterns-app - version 29.14 - styled.components - Features: refactoring QuoteTextScroll styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 29.15 - styled.components
+/**react-design-patterns-app - version 29.14 - styled.components
  * - Features: 
  * 
- *     -->  refactoring 'QuoteBlockScroll' styles to tailwind
+ *     -->  refactoring 'QuoteTextScroll' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -82,7 +82,8 @@ export const CiteContainerScroll = styled.cite`
 `;
 
 export const QuoteTextScroll = styled.p`
-	margin-bottom: 1rem;
+	${tw`mb-4`}
+
 `;
 
 export const QuoteBlockScroll = styled.blockquote`
