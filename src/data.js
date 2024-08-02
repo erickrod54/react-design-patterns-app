ca3076a9e4995222b496b8895a95f1ca5660a54d
Erick Rodriguez <erickrod54@gmail.com>
2024-08-01 21:32:00 -0400

    react-design-patterns-app - version 30.06 - data js - Features: Adding QueryCancellationWithAbortSignal Component id

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 30.05- data js  
+/**react-design-patterns-app - version 30.06- data js  
  * - Features: 
  *    
- *     --> Adding 'QueryCancellationWithAbortSignal Component'    
+ *     --> Adding 'QueryCancellationWithAbortSignal Component' id   
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -5997,7 +5997,7 @@ const UsersApiHookLogicAndDataAbs = () => {
     `
     },
     {
-    id: 167,
+    id: 168,
     name: 'QueryCancellationWithAbortSignal Component',
     code:   
     `
