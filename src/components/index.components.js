908ae680cc2de239cf66ff3cc91cbbf51870748a
Erick Rodriguez <erickrod54@gmail.com>
2024-08-02 21:31:00 -0400

    react-design-patterns-app - version 30.08 - components > index - Features: Importing and exporting QueryCancellationWithAbortSignal

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -10,6 +10,7 @@ import ApiLayerAndAsync from "./api.layer.and.async.operations.component.jsx";
 import SearchMealsComponent from "./api.layer.use.cases.meal.search.component.jsx";
 import SearchMealComponentErrorLogger from "./api.layer.use.cases.meal.search.error.logger.component.jsx";
 import UseCasesMealApiSearch from "./api.layer.use.cases.meal.search.jsx";
+import QueryCancellationWithAbortSignal from "./api.layer.use.cases.quotes.cancellation.queries.jsx";
 import FetchTopQuotes from "./api.layer.use.cases.quotes.component.jsx";
 import InfiniteScrollQuotes from "./api.layer.use.cases.quotes.infinite.scroll.jsx";
 import PaginatedQuotes from "./api.layer.use.cases.quotes.pagination.jsx";
@@ -125,10 +126,10 @@ import WithtKeysExercise from "./withtkeys.exersice.component";
 import Writer from "./writer.use.transition.before.component";
 
 
-/**react-design-patterns-app - version 28.19 - components > index  
+/**react-design-patterns-app - version 30.08 - components > index  
  * - Features: 
  * 
- *     --> Importing and exporting 'InfiniteScrollQuotes'
+ *     --> Importing and exporting 'QueryCancellationWithAbortSignal'
  * 
  * Note: Pending to make responsive design
  */
@@ -258,5 +259,6 @@ export {
          FetchTopQuotes,
          UpdateQuotesForm,
          PaginatedQuotes,
-         InfiniteScrollQuotes
+         InfiniteScrollQuotes,
+         QueryCancellationWithAbortSignal
         }
