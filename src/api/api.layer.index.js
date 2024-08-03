adcde3e70d7417bacb8422290b146fde083f17f7
Erick Rodriguez <erickrod54@gmail.com>
2024-08-02 21:30:00 -0400

    react-design-patterns-app - version 30.08 - Api Layer index - Features: Importing and exporting fetchTopQuotesWithCancellation

--- src/api/api.layer.index.js
+++ src/api/api.layer.index.js
@@ -1,15 +1,21 @@
 import { fetchTopQuotes } from './api.layer.quotes.request';
+import { fetchTopQuotesWithCancellation } from './api.layer.use.case.quotes.cancellation.query';
 import { fetchQuotesByCursor } from './api.layer.use.case.quotes.infinite.scroll';
 import { fetchQuotesByPage } from './api.layer.use.case.quotes.pagination';
 import { postQuote, resetQuotes } from './api.layer.use.case.quotes.post.update';
 
 
-/**react-design-patterns-app - version 28.18 - Api Layer index
+/**react-design-patterns-app - version 30.08 - Api Layer index
  * - Features: 
  * 
- *     --> Importing and exporting 'fetchQuotesByCursor'
+ *     --> Importing and exporting 'fetchTopQuotesWithCancellation'
  * 
  * Note: pending to import and export the rest of the API's 
  */
 
-export {  fetchTopQuotes, postQuote, resetQuotes, fetchQuotesByPage, fetchQuotesByCursor }
+export {  fetchTopQuotes,
+          postQuote,
+          resetQuotes,
+          fetchQuotesByPage,
+          fetchQuotesByCursor,
+          fetchTopQuotesWithCancellation }
