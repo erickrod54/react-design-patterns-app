ca93ecf909edd47f81eaa339b3ab4d824d3a406a
Erick Rodriguez <erickrod54@gmail.com>
2024-08-04 21:34:00 -0400

    react-design-patterns-app - version 30.13 - styled.components - Features: writting FetchButton styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 30.12 - styled.components
+/**react-design-patterns-app - version 30.13 - styled.components
  * - Features: 
  * 
- *     -->  writting 'CheckboxInput' styles 
+ *     -->  writting 'FetchButton' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const FetchButton = styled.button`
+	background-color: #4299e1;
+	color: #fff;
+	padding: 0.75rem 1rem;
+`;
 
 export const CheckboxInput = styled.input`
 	margin-right: 0.75rem;
