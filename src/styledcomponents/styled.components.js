61397c36a31fd459056bf3b7c919d4c783565f70
Erick Rodriguez <erickrod54@gmail.com>
2024-08-06 21:31:00 -0400

    react-design-patterns-app - version 30.16 - styled.components - Features: writting QuotesContainerCancellation styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 30.15 - styled.components
+/**react-design-patterns-app - version 30.16 - styled.components
  * - Features: 
  * 
- *     -->  writting 'LoadingMessageCancellation' styles 
+ *     -->  writting 'QuotesContainerCancellation' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,11 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+export const QuotesContainerCancellation = styled.div`
+  max-height: 24rem;
+  overflow-y: auto;
+  border-top: 1px solid #cbd5e0;
+`;
 
 export const LoadingMessageCancellation = styled.p``;
 
