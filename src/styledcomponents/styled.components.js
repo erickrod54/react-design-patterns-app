b929b3d03b79232582b2f0aa301edd2b50dc9eaa
Erick Rodriguez <erickrod54@gmail.com>
2024-08-07 21:31:00 -0400

    react-design-patterns-app - version 30.18 - styled.components - Features: writting QuoteCancellation styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 30.17 - styled.components
+/**react-design-patterns-app - version 30.18 - styled.components
  * - Features: 
  * 
- *     -->  writting 'QuoteBlockCancellation' styles 
+ *     -->  writting 'QuoteTexCancellation' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,10 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const QuoteTexCancellation = styled.p`
+	margin-bottom: 1rem;
+`;
 
 export const QuoteBlockCancellation = styled.blockquote`
 	position: relative;
