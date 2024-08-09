ed1c7f81653c387f29ff415128007c4aa7c1ef8f
Erick Rodriguez <erickrod54@gmail.com>
2024-08-08 21:31:00 -0400

    react-design-patterns-app - version 31.00 - styled.components - Features: writting AuthorTextCancellation styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 30.19 - styled.components
+/**react-design-patterns-app - version 31.00 - styled.components
  * - Features: 
  * 
- *     -->  writting 'CiteContainerCancellation' styles 
+ *     -->  writting 'AuthorTextCancellation' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,14 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+
+export const AuthorTextCancellation = styled.span`
+  margin-bottom: 1px;
+  font-size: 0.875rem;
+  font-style: italic;
+  font-weight: bold;
+`;
 
 export const CiteContainerCancellation = styled.cite`
 	display: flex;
