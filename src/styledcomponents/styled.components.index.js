a650c99291cabde48d187f0eced19547082f13d6
Erick Rodriguez <erickrod54@gmail.com>
2024-08-08 21:32:00 -0400

    react-design-patterns-app - version 31.00 - styledcomponents > index - Features: Importing and exporting AuthorTextCancellation

--- src/styledcomponents/styled.components.index.js
+++ src/styledcomponents/styled.components.index.js
@@ -3,6 +3,7 @@ import { AlertWrapper,
          AuthorInfo,
          AuthorName,
          AuthorText,
+         AuthorTextCancellation,
          AuthorTextScroll,
          BooksUseTransitionContainer,
          ButtonGeneral,
@@ -86,10 +87,10 @@ import { AlertWrapper,
          TitlePostForm,
          TitleScroll, ToggleTextScroll, ToolTipWrapper, WriterContainer } from "./styled.components";
 
-/**react-design-patterns-app - version 30.19 - styledcomponents > index
+/**react-design-patterns-app - version 31.00 - styledcomponents > index
  * - Features: 
  * 
- *     --> Importing and exporting 'CiteContainerCancellation' 
+ *     --> Importing and exporting 'AuthorTextCancellation' 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -186,4 +187,5 @@ export { SectionWrapper,
          QuoteBlockCancellation,
          QuoteTexCancellation,
          CiteContainerCancellation,
+         AuthorTextCancellation,
          QuoteTextPagination, ButtonPostForm, FormGroup, MealListContainer, MealItem }
