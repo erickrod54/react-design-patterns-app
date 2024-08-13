73fed5743ff39e3f139bd37e0f9584fb12db5a8d
Erick Rodriguez <erickrod54@gmail.com>
2024-08-12 21:30:00 -0400

    react-design-patterns-app - version 31.12 - styled.components - Features: writting state-management-patterns styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 31.05 - styled.components
+/**react-design-patterns-app - version 31.12 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ButtonScrollCancellation' styles 
+ *     -->  writting 'state-management-patterns' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -920,6 +920,11 @@ export const CustomTitleStyle = styled.div`
 		}	
 }
 
+.state-management-patterns{
+	span{
+			${tw`text-indigo-400 underline`}
+		}	
+}
 
 `
 
