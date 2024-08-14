fc3d3ab6e98c1a6e8a26b8fd56ae5225d60d196e
Erick Rodriguez <erickrod54@gmail.com>
2024-08-13 21:31:00 -0400

    react-design-patterns-app - version 31.19 - styled.components - Features: writting QuotesContainerCancellation styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 31.18 - styled.components
+/**react-design-patterns-app - version 31.19 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ErrorMessageCancellation' styles to tailwind
+ *     -->  writting 'QuotesContainerCancellation' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -86,9 +86,8 @@ export const QuoteBlockCancellation = styled.blockquote`
 `;
 
 export const QuotesContainerCancellation = styled.div`
-  max-height: 24rem;
-  overflow-y: auto;
-  border-top: 1px solid #cbd5e0;
+  	${tw`max-h-96 overflow-y-auto border-t border-gray-300`}
+
 `;
 
 export const LoadingMessageCancellation = styled.p``;
