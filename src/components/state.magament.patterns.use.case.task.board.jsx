f0ab9c8b031463d1c004d02db397bf933067e078
Erick Rodriguez <erickrod54@gmail.com>
2024-08-17 21:35:00 -0400

    react-design-patterns-app - version 32.14 - TaskBoard - Features: Building onTaskNameChange handler

--- src/components/state.magament.patterns.use.case.task.board.jsx
+++ src/components/state.magament.patterns.use.case.task.board.jsx
@@ -3,10 +3,10 @@ import { useImmer } from "use-immer";
 import { usePatternsAppContext } from "../context";
 import styled from "styled-components";
 
-/**react-design-patterns-app - version 32.13 - TasksBoard
+/**react-design-patterns-app - version 32.14 - TasksBoard
  * - Features: 
  * 
- *     --> Building 'onSelectTask'  handler
+ *     --> Building 'onTaskNameChange'  handler
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
@@ -25,8 +25,39 @@ const TasksBoard = (props) => {
           columnIdx,
           taskIdx,
         });
+    };
+
+    const onTaskNameChange = (e) => {
+        if (!selectedTask) return;
+        const { columnIdx, taskIdx } = selectedTask;
+    
+          setBoard((board) => {
+            return {
+              ...board,
+              columns: [
+                ...board.columns.map((column, _columnIdx) => {
+                  if (columnIdx !== _columnIdx) {
+                    return column;
+                  }
+                  return {
+                    ...column,
+                    tasks: column.tasks.map((task, _taskIdx) => {
+                      if (taskIdx !== _taskIdx) {
+                        return task;
+                      }
+                      return {
+                        ...task,
+                        name: e.target.value,
+                      };
+                    }),
+                  };
+                }),
+              ],
+            };
+          });
       };
   
+      
     return (
       <></>
     );
