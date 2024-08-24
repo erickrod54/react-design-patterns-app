56dc1203bba6209982fc446467d6bac04fcdf02d
Erick Rodriguez <erickrod54@gmail.com>
2024-08-23 21:35:00 -0400

    react-design-patterns-app - version 33.14 - data js - Features: Adding TaskBoard code

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import { layoutexamples, sidebarexample } from "./assets/index.assets"
 
 
-/**react-design-patterns-app - version 33.13- data js  
+/**react-design-patterns-app - version 33.14- data js  
  * - Features: 
  *    
- *     --> Adding 'TasksBoardImmer - Snippet 3' comments   
+ *     --> Adding 'TasksBoardImmer code'   
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -6492,6 +6492,85 @@ const UsersApiHookLogicAndDataAbs = () => {
         );
       };
       export default TasksBoardImmer;
+    `
+    },
+    {
+    id: 176,
+    name: ' TasksBoardImmer code',
+    code:   
+    `
+    const TasksBoardImmer = (props) => {
+    
+    const { boardData } = usePatternsAppContext();
+
+    const [board, setBoard] = useImmer(boardData);
+    const [selectedTask, setSelectedTask] = useState();
+
+    const onSelectTask = (columnIdx, taskIdx) => {
+        setSelectedTask({
+          columnIdx,
+          taskIdx,
+        });
+    };
+
+    const onTaskNameChange = (e) => {
+        if (!selectedTask) return;
+        const { columnIdx, taskIdx } = selectedTask;
+    
+        setBoard((board) => {
+          board.columns[columnIdx].tasks[taskIdx].name = e.target.value;
+        });
+    };
+      
+      
+      return (
+          <Container>
+              <Header>
+                  <HeaderBackground>
+                  <BoardTitle>{board.name}</BoardTitle>
+                  </HeaderBackground>
+                  <Content>
+                  {board.columns.map((column, columnIdx) => (
+                      <ColumnContainer key={columnIdx}>
+                      <ColumnHeader>{column.name}</ColumnHeader>
+                      <TaskContainer>
+                          {column.tasks.map((task, taskIdx) => (
+                          <TaskButton
+                              key={taskIdx}
+                              isSelected={
+                              columnIdx === selectedTask?.columnIdx &&
+                              taskIdx === selectedTask?.taskIdx
+                              }
+                              onClick={() => onSelectTask(columnIdx, taskIdx)}
+                          >
+                              <h4>{task.name}</h4>
+                          </TaskButton>
+                          ))}
+                      </TaskContainer>
+                      </ColumnContainer>
+                  ))}
+                  <div>
+                      <UpdateTaskHeader>
+                      {selectedTask ? "Update task" : "Select a task to update"}
+                      </UpdateTaskHeader>
+                      {selectedTask ? (
+                      <UpdateTaskInput
+                          type="text"
+                          value={
+                          board.columns[selectedTask.columnIdx].tasks[
+                              selectedTask.taskIdx
+                          ].name
+                          }
+                          onChange={onTaskNameChange}
+                      />
+                      ) : null}
+                  </div>
+                  </Content>
+              </Header>
+          </Container>
+      );
+    };
+    export default TasksBoardImmer;
     `
     }
   ];
