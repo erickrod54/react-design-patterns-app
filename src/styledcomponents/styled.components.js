1ff2b858e736cbba3f1ce33026d1af7e9c74d872
Erick Rodriguez <erickrod54@gmail.com>
2024-08-25 21:33:00 -0400

    react-design-patterns-app - version 33.20 - styled.components - Features: writting HeaderTaskBoard styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 33.19 - styled.components
+/**react-design-patterns-app - version 33.20 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ContainerTasksBoard' styles to tailwind
+ *     -->  writting 'HeaderTaskBoard' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,10 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const HeaderTaskBoard = styled.div`
+	text-align: left;
+`;
 
 export const ContainerTasksBoard = styled.div`
   padding: 2rem 0;
