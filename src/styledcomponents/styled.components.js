678aba7bbe1db6a82e21f5045aee165bddefac1c
Erick Rodriguez <erickrod54@gmail.com>
2024-08-27 21:33:00 -0400

    react-design-patterns-app - version 34.03 - styled.components - Features: writting ColumnContainerTaskBoard styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.02 - styled.components
+/**react-design-patterns-app - version 34.03 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ContentTaskBoard' styles to tailwind
+ *     -->  writting 'ColumnContainerTaskBoard' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -17,6 +17,11 @@ import tw from 'twin.macro';
  * to use them in the custom titles
  */
 
+export const ColumnContainerTaskBoard = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-right: 1.5rem;
+`;
 export const ContentTaskBoard = styled.div`
 	display: flex;
 	gap: 1rem;
