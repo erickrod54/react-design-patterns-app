66c51e8b91db901b06d521ea1d6fdb0340f31372
Erick Rodriguez <erickrod54@gmail.com>
2024-08-30 21:30:00 -0400

    react-design-patterns-app - version 34.00 - styled.components - Features: UpdateTaskInputBaord styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.07 - styled.components
+/**react-design-patterns-app - version 34.08 - styled.components
  * - Features: 
  * 
- *     -->  writting 'UpdateTaskHeaderBoard' styles 
+ *     -->  writting 'UpdateTaskInputBoard' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const UpdateTaskInputBoard = styled.input`
+	width: 100%;
+	padding: 0.5rem;
+	border: 1px solid #466997;
+	color: #000;
+`;
 
 export const UpdateTaskHeaderBoard = styled.h2`
 	font-weight: 600;
