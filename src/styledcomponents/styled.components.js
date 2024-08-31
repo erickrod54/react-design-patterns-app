a25ceeae0ba9f069aa16458d1dbec6792d5d3d44
Erick Rodriguez <erickrod54@gmail.com>
2024-08-30 21:33:00 -0400

    react-design-patterns-app - version 34.09 - styled.components - Features: writting ContainerImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.08 - styled.components
+/**react-design-patterns-app - version 34.09 - styled.components
  * - Features: 
  * 
- *     -->  writting 'UpdateTaskInputBoard' styles 
+ *     -->  writting 'ContainerImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const ContainerImmer = styled.div`
+	padding: 2rem 0;
+	max-width: 4xl;
+	margin: auto;
+`;
 
 export const UpdateTaskInputBoard = styled.input`
 	width: 100%;
