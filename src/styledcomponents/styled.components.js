6ef081822dc8744b1a4f84ebc82ea9ef42157f7d
Erick Rodriguez <erickrod54@gmail.com>
2024-09-01 21:30:00 -0400

    react-design-patterns-app - version 34.12 - styled.components - Features: writting BoardTitleImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.11 - styled.components
+/**react-design-patterns-app - version 34.12 - styled.components
  * - Features: 
  * 
- *     -->  writting 'HeaderBackgroundImmer' styles 
+ *     -->  writting 'BoardTitleImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,11 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const BoardTitleImmer = styled.h2`
+	font-weight: bold;
+	color: #080707;
+`;
 
 export const HeaderBackgroundImmer = styled.div`
 	background-color: #348bd3;
