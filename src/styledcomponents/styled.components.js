19c12a283ba188daf4115da0a311159cb1f3e19f
Erick Rodriguez <erickrod54@gmail.com>
2024-09-02 21:33:00 -0400

    react-design-patterns-app - version 34.15 - styled.components - Features: writting ColumnHeaderImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.14 - styled.components
+/**react-design-patterns-app - version 34.15 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ColumnContainerImmer' styles 
+ *     -->  writting 'ColumnHeaderImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const ColumnHeaderImmer = styled.h3`
+	font-weight: 600;
+	margin-bottom: 0.75rem;
+	color: aliceblue;
+`;
 
 export const ColumnContainerImmer = styled.div`
 	display: flex;
