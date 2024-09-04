f2be4bc05827076142c6051d9d0d2f37c6aa6625
Erick Rodriguez <erickrod54@gmail.com>
2024-09-03 21:30:00 -0400

    react-design-patterns-app - version 34.17 - styled.components - Features: writting TaskContainerImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.15 - styled.components
+/**react-design-patterns-app - version 34.17 - styled.components
  * - Features: 
  * 
- *     -->  writting 'ColumnHeaderImmer' styles 
+ *     -->  writting 'TaskContainerImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const TaskContainerImmer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.75rem;
+	height: 70vh;
+`;
 
 export const ColumnHeaderImmer = styled.h3`
 	font-weight: 600;
