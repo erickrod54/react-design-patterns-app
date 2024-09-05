babfcb4eb1729ff30b3f7b3a357489c477901851
Erick Rodriguez <erickrod54@gmail.com>
2024-09-04 21:33:00 -0400

    react-design-patterns-app - version 34.20 - styled.components - Features: writting UpdateTaskInputImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.19 - styled.components
+/**react-design-patterns-app - version 34.20 - styled.components
  * - Features: 
  * 
- *     -->  writting 'UpdateTaskHeaderImmer' styles 
+ *     -->  writting 'UpdateTaskInputImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const UpdateTaskInputImmer = styled.input`
+	width: 100%;
+	padding: 0.5rem;
+	border: 1px solid #466997;
+	color: #000;
+`;
 
 export const UpdateTaskHeaderImmer = styled.h2`
 	font-weight: 600;
