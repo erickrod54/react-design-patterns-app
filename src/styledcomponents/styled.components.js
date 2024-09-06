13583643c9f1e4a588d591c05d91df1e1e958584
Erick Rodriguez <erickrod54@gmail.com>
2024-09-05 21:30:00 -0400

    react-design-patterns-app - version 35.00 - styled.components - Features: Refactoring ContainerTaskBoard styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 34.20 - styled.components
+/**react-design-patterns-app - version 35.00 - styled.components
  * - Features: 
  * 
- *     -->  writting 'UpdateTaskInputImmer' styles 
+ *     --> Refactoring 'ContainerTasksBoard' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -158,9 +158,7 @@ export const HeaderTaskBoard = styled.div`
 `;
 
 export const ContainerTasksBoard = styled.div`
-  padding: 2rem 0;
-  max-width: 4xl;
-  margin: auto;
+  	${tw`py-8 max-w-4xl mx-auto`}
 `;
 
 export const ButtonScrollCancellation = styled.button`
