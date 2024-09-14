e09b1440886412ebf4cc006fbd9202e7775b5d19
Erick Rodriguez <erickrod54@gmail.com>
2024-09-13 21:32:00 -0400

    react-design-patterns-app -  version 36.16 - ShoppingListBefore - Features: Building ShoppingListBefore data

---
+++ src/components/state.magament.patterns.use.case.shopping.list.jsx
@@ -0,0 +1,42 @@
+import React, { useReducer } from "react";
+import styled from "styled-components";
+import ShoppingListHeaderBefore from "./state.magament.patterns.use.case.shopping.list.header";
+import ShoppingListRowBefore from "./state.magament.patterns.use.case.shopping.list.row";
+
+/**react-design-patterns-app - version 36.16 - ShoppingListBefore
+ * - Features: 
+ * 
+ *     -->  Building 'ShoppingListBefore' data
+ * 
+ * Note: the macro 'tw' in order to work and use has to be installed
+ * there is around 4 or 5 libraries and this is oen of the most active
+ * and recent
+ * 
+ *          https://www.npmjs.com/package/twin.macro
+ * 
+ * i have to create a root access custom color palette in order 
+ * to use them in the custom titles
+ */
+
+
+
+//Initial state for the shopping list reducer
+
+const shoppingItems = {
+  newShoppingItemName: "",
+  items: [
+    {
+      id: "1",
+      name: "Sea Salt",
+    },
+    {
+      id: "2",
+      name: "Apples",
+    },
+    {
+      id: "3",
+      name: "Chicken breasts",
+    },
+  ],
+};
+
