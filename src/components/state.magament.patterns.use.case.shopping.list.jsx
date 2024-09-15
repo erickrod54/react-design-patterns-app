50ddad86d95d95ee10b043eaf5467d8d6961ce5a
Erick Rodriguez <erickrod54@gmail.com>
2024-09-14 21:33:00 -0400

    react-design-patterns-app - version 36.20 - ShoppingListBefore - Features: Building reducer actions

--- src/components/state.magament.patterns.use.case.shopping.list.jsx
+++ src/components/state.magament.patterns.use.case.shopping.list.jsx
@@ -3,19 +3,20 @@ import styled from "styled-components";
 import ShoppingListHeaderBefore from "./state.magament.patterns.use.case.shopping.list.header";
 import ShoppingListRowBefore from "./state.magament.patterns.use.case.shopping.list.row";
 
-/**react-design-patterns-app - version 36.16 - ShoppingListBefore
+/**react-design-patterns-app - version 36.20 - ShoppingListBefore
  * - Features: 
  * 
- *     -->  Building 'ShoppingListBefore' data
+ *     -->  Building 'reducer' actions
  * 
- * Note: the macro 'tw' in order to work and use has to be installed
- * there is around 4 or 5 libraries and this is oen of the most active
- * and recent
+ * Note: the reducer actions will implement later the 
+ * useReducer hook in  order to perform the following
+ * 4 operations:
  * 
- *          https://www.npmjs.com/package/twin.macro
- * 
- * i have to create a root access custom color palette in order 
- * to use them in the custom titles
+ *     -->  UPDATE_NEW_SHOPPING_ITEM_NAME
+ *     -->  ADD_ITEM
+ *     -->  UPDATE_ITEM
+ *     -->  DELETE_ITEM
+ *    
  */
 
 
@@ -38,5 +39,43 @@ const shoppingItems = {
       name: "Chicken breasts",
     },
   ],
+};
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+
+    case "UPDATE_NEW_SHOPPING_ITEM_NAME":
+      return {
+        ...state,
+        newShoppingItemName: action.payload,
+      };
+
+    case "ADD_ITEM":
+      return {
+        ...state,
+        newShoppingItemName: "",
+        items: [...state.items, action.payload],
+      };
+
+    case "UPDATE_ITEM":
+      return {
+        ...state,
+        items: state.items.map((item, idx) => {
+          if (idx === action.payload.index) {
+            return action.payload.item;
+          }
+          return item;
+        }),
+      };
+
+    case "DELETE_ITEM":
+      return {
+        ...state,
+        items: state.items.filter((_, idx) => idx !== action.payload.index),
+      };
+    default:
+      return state;
+  }
 };
 
