93a834bdc99cb889baa6334b5f466d5adbe262d3
Erick Rodriguez <erickrod54@gmail.com>
2024-09-15 21:33:00 -0400

    react-design-patterns-app - version 37.03 - ShoppingListBefore - Features: Building ShoppingListBefore

--- src/components/state.magament.patterns.use.case.shopping.list.jsx
+++ src/components/state.magament.patterns.use.case.shopping.list.jsx
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import ShoppingListHeaderBefore from "./state.magament.patterns.use.case.shopping.list.header";
 import ShoppingListRowBefore from "./state.magament.patterns.use.case.shopping.list.row";
 
-/**react-design-patterns-app - version 36.20 - ShoppingListBefore
+/**react-design-patterns-app - version 37.03 - ShoppingListBefore
  * - Features: 
  * 
- *     -->  Building 'reducer' actions
+ *     -->  Building 'ShoppingListBefore' 
  * 
  * Note: the reducer actions will implement later the 
  * useReducer hook in  order to perform the following
@@ -78,4 +78,71 @@ const reducer = (state, action) => {
       return state;
   }
 };
+
+const ShoppingListBefore = (props) => {
+
+  const [shoppingList, dispatch] = useReducer(reducer, shoppingItems);
+
+  const addItem = () => {
+    if (!shoppingList.newShoppingItemName) return;
+    dispatch({
+      type: "ADD_ITEM",
+      payload: {
+        id: getUuid(),
+        name: shoppingList.newShoppingItemName,
+      },
+    });
+  };
+
+  const deleteItem = (item) => {
+    dispatch({
+      type: "DELETE_ITEM",
+      payload: item,
+    });
+  };
+
+  const updateItem = (payload) => {
+    dispatch({
+      type: "UPDATE_ITEM",
+      payload,
+    });
+  };
+
+  const onChangeShoppingListItemName = (e) => {
+    dispatch({
+      type: "UPDATE_NEW_SHOPPING_ITEM_NAME",
+      payload: e.target.value,
+    });
+  };
+
+  return (
+    <StyledContainer>
+      <StyledWrapper>
+        <ShoppingListHeaderBefore shoppingList={shoppingList.items} />
+        <div style={{ marginBottom: "1.5rem" }}>
+          {shoppingList.items.map((item, index) => (
+            <ShoppingListRowBefore
+              key={item.id}
+              item={item}
+              index={index}
+              updateItem={updateItem}
+              deleteItem={deleteItem}
+            />
+          ))}
+        </div>
+        <StyledAddItemContainer>
+          <StyledLabel htmlFor="shoppingItemField">Add item</StyledLabel>
+          <StyledInput
+            type="text"
+            id="shoppingItemField"
+            value={shoppingList.newShoppingItemName}
+            onChange={onChangeShoppingListItemName}
+          />
+          <StyledButton onClick={addItem}>Add</StyledButton>
+        </StyledAddItemContainer>
+      </StyledWrapper>
+    </StyledContainer>
+  );
+};
+export default ShoppingListBefore;
 
