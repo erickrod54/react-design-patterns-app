3a174e6d30fc6e07915120009e7bc97e3af49336
Erick Rodriguez <erickrod54@gmail.com>
2024-09-19 21:30:00 -0400

    react-desing-patterns-app - version 37.15 - ShoppingListImmer - Features: Building reducer

--- src/components/state.magament.patterns.use.case.shopping.list.immer.jsx
+++ src/components/state.magament.patterns.use.case.shopping.list.immer.jsx
@@ -2,11 +2,12 @@ import React, { useReducer } from "react";
 import styled from "styled-components";
 import ShoppingListHeaderBefore from "./state.magament.patterns.use.case.shopping.list.header";
 import ShoppingListRowBefore from "./state.magament.patterns.use.case.shopping.list.row";
+import { useImmerReducer } from "use-immer";
 
-/**react-design-patterns-app - version 37.14- ShoppingListImmer
+/**react-design-patterns-app - version 37.15- ShoppingListImmer
  * - Features: 
  * 
- *     -->  Building 'ShoppingListImmer'  
+ *     -->  Building 'reducer'  
  * 
  * Note: the reducer actions will implement later the 
  * useImmerReducer hook in  order to perform the following
@@ -39,6 +40,37 @@ const shoppingItemsImmer = {
     },
   ],
 };
+
+/**this reducer has actions types with syntax
+ * according with useImmer, dynamic object asignation*/
+const reducer = (state, action) => {
+
+    switch (action.type) {
+  
+     // Update the name of the new shopping item
+     case "UPDATE_NEW_SHOPPING_ITEM_NAME":
+        state.newShoppingItemName = action.payload;
+        break;
+
+    // Add a new item to the shopping list
+    case "ADD_ITEM":
+        state.newShoppingItemName == "";
+        state.items.push(action.payload);
+        break;
+
+    // Update an existing item in the shopping list
+    case "UPDATE_ITEM":
+        state.items.splice(action.payload.index, 1, action.payload.item);
+        break;
+
+    // Delete an item from the shopping list
+    case "DELETE_ITEM":
+        state.items.splice(action.payload.index, 1);
+        break;
+        
+    }
+    return state;
+  };
 
 
 
