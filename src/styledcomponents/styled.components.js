0f5b52a3a5615e9187646440766ed9fac4b4add9
Erick Rodriguez <erickrod54@gmail.com>
2024-09-25 21:30:00 -0400

    react-design-patterns-app - version 38.16 - styled.components - Features: writting StyledLabelReducer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 38.15 - styled.components
+/**react-design-patterns-app - version 38.16 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledAddItemContainerReducer' styles 
+ *     --> writting 'StyledLabelReducer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledLabelReducer = styled.label`
+	margin-bottom: 0.5rem;
+	color: yellow;
+	font-size: 1.5rem;
+`;
 
 export const StyledAddItemContainerReducer = styled.div`
 	display: flex;
