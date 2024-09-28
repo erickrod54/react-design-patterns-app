d227fdac31a8433e50891e822b35d22298c207b8
Erick Rodriguez <erickrod54@gmail.com>
2024-09-27 21:31:00 -0400

    react-design-patterns-app - version 38.20 - reducerone - Features: Migrating reducerone actions

---
+++ src/actions/actions.index.js
@@ -0,0 +1,19 @@
+import { reducerone } from "./shopping.list.reducer.actions";
+
+/**react-design-patterns-app - version 38.20 - actions > index
+ * - Features: 
+ * 
+ *     -->  Migrating 'reducerone' data 
+ * 
+ * Note: the reducer actions will implement later the 
+ * useReducer hook in  order to perform the following
+ * 4 operations:
+ * 
+ *     -->  UPDATE_NEW_SHOPPING_ITEM_NAME
+ *     -->  ADD_ITEM
+ *     -->  UPDATE_ITEM
+ *     -->  DELETE_ITEM
+ *    
+ */
+
+export { reducerone }
