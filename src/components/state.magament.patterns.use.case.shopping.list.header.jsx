61529b727451c5feaad507e85bbc9a89fef64f1f
Erick Rodriguez <erickrod54@gmail.com>
2024-09-28 21:34:00 -0400

    react-design-patterns-app - version 39.02 - ShoppingListHeaderBefore - Features: Migrating StyleHeadingBefore styles

--- src/components/state.magament.patterns.use.case.shopping.list.header.jsx
+++ src/components/state.magament.patterns.use.case.shopping.list.header.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import { StyledContainerBefore } from "../styledcomponents/styled.components";
+import { StyledContainerBefore, StyledHeadingBefore } from "../styledcomponents/styled.components";
 
-/**react-design-patterns-app - version 39.01 - ShoppingListHeaderBefore
+/**react-design-patterns-app - version 39.02 - ShoppingListHeaderBefore
  * - Features: 
  * 
- *     -->  Migrating 'StyledContainerBefore' styles
+ *     -->  Migrating 'StyledHeadingBefore' styles
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -18,15 +18,10 @@ import { StyledContainerBefore } from "../styledcomponents/styled.components";
 
 
 
-const StyledHeading = styled.h2`
-  font-weight: bold;
-  margin-right: 2rem;
-`;
-
 const ShoppingListHeaderBefore = (props) => {
   return (
     <StyledContainerBefore>
-        <StyledHeading>Shopping List</StyledHeading>
+        <StyledHeadingBefore>Shopping List</StyledHeadingBefore>
         <span>{props.shoppingList.length} items 🛒</span>
     </StyledContainerBefore>
   );
