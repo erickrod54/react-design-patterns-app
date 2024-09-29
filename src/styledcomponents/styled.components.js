f7039d9c4bb4b29372b91689e7337fc660fb1089
Erick Rodriguez <erickrod54@gmail.com>
2024-09-28 21:32:00 -0400

    react-design-patterns-app - version 39.02 - styled.components - Features: writting StyledHeadingBefore styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 39.01 - styled.components
+/**react-design-patterns-app - version 39.02 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledContainerBefore' styles 
+ *     --> writting 'StyledHeadingBefore' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,11 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledHeadingBefore = styled.h2`
+	font-weight: bold;
+	margin-right: 2rem;
+`;
 
 export const StyledContainerBefore = styled.div`
 	margin-bottom: 1.5rem;
