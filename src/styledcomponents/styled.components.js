4770684c5c52c7035f431319f4dfdabd7b1e0cc1
Erick Rodriguez <erickrod54@gmail.com>
2024-09-28 21:35:00 -0400

    react-design-patterns-app - version 39.03 - styled.components - Features: writting StyledContainerImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 39.02 - styled.components
+/**react-design-patterns-app - version 39.03 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledHeadingBefore' styles 
+ *     --> writting 'StyledContainerImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledContainerImmer = styled.div`
+	padding-top: 2rem;
+	max-width: 4xl;
+	margin: 0 auto;
+	text-align: left;
+`;
 
 export const StyledHeadingBefore = styled.h2`
 	font-weight: bold;
