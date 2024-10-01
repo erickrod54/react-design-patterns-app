a0c24fd13fa3278e49832475fdb9be157db69bbf
Erick Rodriguez <erickrod54@gmail.com>
2024-09-30 21:32:00 -0400

    react-design-patterns-app - version 39.06 - styled.components - Features: writting StyledLabelImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 39.05 - styled.components
+/**react-design-patterns-app - version 39.06 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledAddItemContainerImmer' styles 
+ *     --> writting 'StyledLabelImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledLabelImmer = styled.label`
+	margin-bottom: 0.5rem;
+	color: yellow;
+	font-size: 1.5rem;
+`;
 
 export const StyledAddItemContainerImmer = styled.div`
 	display: flex;
