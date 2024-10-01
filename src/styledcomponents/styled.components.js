6fc553ef25b1af6e67eacf900c90832fbd753633
Erick Rodriguez <erickrod54@gmail.com>
2024-09-30 21:35:00 -0400

    react-design-patterns-app - version 39.07 - styled.components - Features: writting StyledInputImmer styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 39.06 - styled.components
+/**react-design-patterns-app - version 39.07 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledLabelImmer' styles 
+ *     --> writting 'StyledInputImmer' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,15 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledInputImmer = styled.input`
+	width: 100%;
+	margin: 4px;
+	padding: 4px;
+	border-radius: 0.30rem;
+	color: black;
+	font-size: 1.5rem;
+`;
 
 export const StyledLabelImmer = styled.label`
 	margin-bottom: 0.5rem;
