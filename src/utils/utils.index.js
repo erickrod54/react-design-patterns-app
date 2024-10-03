83343c77a73087b8bd0baadcd44a041b1751ada9
Erick Rodriguez <erickrod54@gmail.com>
2024-10-02 21:33:00 -0400

    react-design-patterns-app - version 39.10 - getUuid - Features: Migrating getUuid util

---
+++ src/utils/utils.index.js
@@ -0,0 +1,19 @@
+import { getUuid } from "./shopping.list.utils";
+
+/**react-design-patterns-app - version 39.10 - getUuid
+ * - Features: 
+ * 
+ *     --> Migrating 'getUuid' util 
+ * 
+ * Note: the macro 'tw' in order to work and use has to be installed
+ * there is around 4 or 5 libraries and this is oen of the most active
+ * and recent
+ * 
+ *          https://www.npmjs.com/package/twin.macro
+ * 
+ * i have to create a root access custom color palette in order 
+ * to use them in the custom titles
+ */
+
+export { getUuid }
+
