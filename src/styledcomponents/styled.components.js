f2b7e573c5cf6e36ce271bc96842d4a34cd86733
Erick Rodriguez <erickrod54@gmail.com>
2024-10-04 21:35:00 -0400

    react-design-patterns-app - version 39.19 - styled.components - Features: writting StyledHeadingBefore styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 39.18 - styled.components
+/**react-design-patterns-app - version 39.19 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledButtonReducer' styles to tailwind
+ *     --> writting 'StyledHeadingBefore' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -69,8 +69,8 @@ export const StyledContainerImmer = styled.div`
 `;
 
 export const StyledHeadingBefore = styled.h2`
-	font-weight: bold;
-	margin-right: 2rem;
+	${tw`font-bold mr-8`}
+
 `;
 
 export const StyledContainerBefore = styled.div`
