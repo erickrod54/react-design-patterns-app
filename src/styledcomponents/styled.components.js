33342139acab075f3015cfba6e63fffd61945973
Erick Rodriguez <erickrod54@gmail.com>
2024-10-05 21:31:00 -0400

    react-design-patterns-app - version 39.21 - styled.components - Features: writting StyledWrapperImmer styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 39.20 - styled.components
+/**react-design-patterns-app - version 39.21 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledContainerImmer' styles to tailwind
+ *     --> writting 'StyledWrapperImmer' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -58,7 +58,8 @@ export const StyledAddItemContainerImmer = styled.div`
 `;
 
 export const StyledWrapperImmer = styled.div`
-	max-width: xs;
+	${tw`max-w-4xl`}
+
 `;
 
 export const StyledContainerImmer = styled.div`
