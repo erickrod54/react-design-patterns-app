f8cb6e0935d146d50f5a69670750e11603ce9310
Erick Rodriguez <erickrod54@gmail.com>
2024-10-06 21:34:00 -0400

    react-design-patterns-app - version 40.06 - Home - Features: Building Home

---
+++ src/components/optimization.patterns.home.use.case.jsx
@@ -0,0 +1,12 @@
+/**react-design-patterns-app - version 40.06 - Home - 
+ * - Features: 
+ * 
+ *     --> Building 'Home'
+ * 
+ * Note: This is the first topic
+ */
+
+const Home = (props) => {
+  return <div>Home</div>;
+};
+export default Home;
