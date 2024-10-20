862dd475c8deafa481c579f459f8fa3dfac7ac38
Erick Rodriguez <erickrod54@gmail.com>
2024-10-19 21:30:00 -0400

    react-design-patterns-app - version 42.09 - styled.components - Features: writting HeadingNavigation styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 42.08 - styled.components
+/**react-design-patterns-app - version 42.09 - styled.components
  * - Features: 
  * 
- *     --> writting 'AppContainerNavigation' styles 
+ *     --> writting 'HeadingNavigation' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,11 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const HeadingNavigation = styled.h1`
+	font-weight: 600;
+	font-size: 2xl;
+`;
 
 export const AppContainerNavigation = styled.div`
 	margin: 0 auto;
