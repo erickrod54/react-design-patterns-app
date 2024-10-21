74f45fbe129b90688f3d1b5940964da2ab78f65b
Erick Rodriguez <erickrod54@gmail.com>
2024-10-20 21:30:00 -0400

    react-design-patterns-app - version 42.12 - styled.components - Features: writting Nav styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 42.11 - styled.components
+/**react-design-patterns-app - version 42.12 - styled.components
  * - Features: 
  * 
- *     --> writting 'NavContainer' styles 
+ *     --> writting 'Nav' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,10 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+export const Nav = styled.nav`
+	display: flex;
+	justify-content: space-around;
+`;
 
 export const NavContainer = styled.div`
 	margin-top: 8rem;
