bd136dcf0420d5e462d7f473821a0cc4b577a9d4
Erick Rodriguez <erickrod54@gmail.com>
2024-10-20 21:33:00 -0400

    react-design-patterns-app - version 42.13 - styled.components - Features: writting AppContainerOptimized styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 42.12 - styled.components
+/**react-design-patterns-app - version 42.13 - styled.components
  * - Features: 
  * 
- *     --> writting 'Nav' styles 
+ *     --> writting 'AppContainerOptimized' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,14 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const AppContainerOptimized = styled.div`
+	margin: 0 auto;
+	max-width: 6xl;
+	text-align: center;
+	margin-top: 8rem;
+`;
+
 export const Nav = styled.nav`
 	display: flex;
 	justify-content: space-around;
