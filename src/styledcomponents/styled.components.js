8cf54db94d9fcd66b6d5756405c56fd2b1017c73
Erick Rodriguez <erickrod54@gmail.com>
2024-10-23 21:30:00 -0400

    react-design-patterns-app - version 42.20 - styled.components - Features: writting Nav styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 42.19 - styled.components
+/**react-design-patterns-app - version 42.20 - styled.components
  * - Features: 
  * 
- *     --> writting 'NavContainer' styles to tailwind
+ *     --> writting 'Nav' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -39,8 +39,8 @@ export const AppContainerOptimized = styled.div`
 `;
 
 export const Nav = styled.nav`
-	display: flex;
-	justify-content: space-around;
+	${tw`flex justify-around`}	
+
 `;
 
 export const NavContainer = styled.div`
