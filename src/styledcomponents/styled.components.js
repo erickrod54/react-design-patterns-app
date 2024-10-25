ce1a797d286f8422ac2fd1a003308744ce043fea
Erick Rodriguez <erickrod54@gmail.com>
2024-10-24 21:30:00 -0400

    react-design-patterns-app - version 43.04 - styled.components - Features: writting rendering-optimization styles to tailwind

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 43.03 - styled.components
+/**react-design-patterns-app - version 43.04 - styled.components
  * - Features: 
  * 
- *     --> writting 'NavOptimized' styles to tailwind
+ *     --> writting 'rendering-optimization' styles to tailwind
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -1186,6 +1186,10 @@ export const CustomTitleStyle = styled.div`
 	span{
 			${tw`text-indigo-500 underline`}
 		}
+}
+
+.rendering-optimization{
+	${tw`text-fuchsia-500 underline`}
 }
 
 `
