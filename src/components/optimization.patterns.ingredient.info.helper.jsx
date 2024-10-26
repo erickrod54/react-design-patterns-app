ede52d98f69e5a7c1efa007062ea94a394de2048
Erick Rodriguez <erickrod54@gmail.com>
2024-10-25 21:30:00 -0400

    react-design-patterns-app - version 43.06 - components > index - Features: writting StyledButton fro IngredientsHelper

---
+++ src/components/optimization.patterns.ingredient.info.helper.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+/**react-design-patterns-app - version 43.06 - components > index  
+ * - Features: 
+ * 
+ *     --> writting  'StyledButton'
+ * 
+ * Note: Pending to make responsive design
+ */
+
+const StyledButton = styled.button`
+  width: 1.25rem;
+  height: 1.25rem;
+  border: 0;
+  background-color: white;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
