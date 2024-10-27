af925dec8374b63ea0825b87939c27272df21304
Erick Rodriguez <erickrod54@gmail.com>
2024-10-26 21:33:00 -0400

    react-design-patterns-app - version 43.11 - IngredientsComponent - Features: Building IngredientsComponent

--- src/components/optimization.patterns.ingredient.component.jsx
+++ src/components/optimization.patterns.ingredient.component.jsx
@@ -3,10 +3,10 @@ import { nanoid } from "nanoid";
 import styled from "styled-components";
 
 
-/**react-design-patterns-app - version 43.10 - IngredientsComponent  
+/**react-design-patterns-app - version 43.11 - IngredientsComponent  
  * - Features: 
  * 
- *     --> Building  'initialIngredients' data
+ *     --> Building  'IngredientsComponent' component
  * 
  * Note: Pending to make responsive design
  */
@@ -54,3 +54,52 @@ const initialIngredients = [
       name: "1 tbsp salt",
     },
   ];
+
+  const IngredientsComponent = (props) => {
+    console.log("Ingredient rendered");
+    const [ingredient, setIngredient] = useState("");
+    const [ingredients, setIngredients] = useState(initialIngredients);
+  
+    const addIngredient = (ingredient) => {
+      setIngredients((ingredients) => [
+        ...ingredients,
+        {
+          name: ingredient,
+          id: nanoid(),
+        },
+      ]);
+    };
+  
+    const deleteIngredient = (id) => {
+      setIngredients((ingredients) => ingredients.filter((ing) => ing.id !== id));
+    };
+  
+    const createIngredientsHeaderText = () => {
+      console.log("createIngredientsHeaderText called");
+      return <StyledHeading2>Ingredients ({ingredients.length})</StyledHeading2>;
+    };
+  
+    return (
+      <StyledContainer>
+        <div>
+          {createIngredientsHeaderText()}
+          <IngredientsInfoHelper />
+        </div>
+  
+        <StyledSpaceY4>
+          <IngredientsList
+            ingredients={ingredients}
+            deleteIngredient={deleteIngredient}
+          />
+  
+          <AddIngredient
+            addIngredient={addIngredient}
+            ingredient={ingredient}
+            setIngredient={setIngredient}
+          />
+        </StyledSpaceY4>
+      </StyledContainer>
+    );
+  };
+  
+  export default IngredientsComponent;
