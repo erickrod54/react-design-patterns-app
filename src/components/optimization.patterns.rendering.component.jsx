638002f2f41a95d96fc0daed207b66772f3d92dc
Erick Rodriguez <erickrod54@gmail.com>
2024-10-28 21:32:00 -0400

    react-design-patterns-app - version 43.20 - RenderingOptimization - Features: Starting memo and useCallback

--- src/components/optimization.patterns.rendering.component.jsx
+++ src/components/optimization.patterns.rendering.component.jsx
@@ -4,10 +4,10 @@ import CustomTitle from "./custom.title.component";
 import { usePatternsAppContext } from "../context";
 
 
-/**react-design-patterns-app - version 43.03 - RenderingOptimization
+/**react-design-patterns-app - version 43.20 - RenderingOptimization
  * - Features: 
  * 
- *     --> Building 'RenderingOptimization'     
+ *     --> Starting  'memo and useCallback'     
  * 
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
@@ -25,8 +25,21 @@ const RenderingOptimization = () => {
 
             <SectionWrapper>
 
-    
+            <p>
+            <span className="text-white font-semibold"> Fast Rendering </span>  is part of a rich user experience, in order to achieve this experience 
+                it will be covered two patterns:
+            </p>
 
+            <ul className="list-white-space">
+                <li>
+                    <span className="text-white font-semibold"> memo </span> (  Optimizes React components by memoizing them, preventing unnecessary re-renders if their props haven't changed. )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> useCallback </span> ( Memoizes a function to maintain its reference between renders, preventing it from being recreated unless dependencies change. )
+                </li>
+            </ul>
+
+    
             </SectionWrapper>
         </div>
 )
