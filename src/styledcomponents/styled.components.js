0f5bf2e8a620e0d8feefab317809515fc12a9e0e
Erick Rodriguez <erickrod54@gmail.com>
2024-11-06 21:35:00 -0500

    react-design-patterns-app - version 45.12 - styled.components - Features: writting StyledSpaceY4Ingredients styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 45.11 - styled.components
+/**react-design-patterns-app - version 45.12 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledHeadingIngredients' styles 
+ *     --> writting 'StyledSpaceY4Ingredients' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,14 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledSpaceY4Ingredients = styled.div`
+	margin-top: 1rem;
+
+	> * + * {
+		margin-top: 1rem;
+	}
+`;
 
 export const StyledHeadingIngredients = styled.h2`
 	margin-bottom: 1rem;
