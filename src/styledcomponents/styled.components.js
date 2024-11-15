1450e9ef3c92f504022de2df54fedb57a1415ad4
Erick Rodriguez <erickrod54@gmail.com>
2024-11-14 21:35:00 -0500

    react-design-patterns-app - version 46.08 - styled.components - Features: writting StyledSpaceY4Optimized styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 46.07 - styled.components
+/**react-design-patterns-app - version 46.08 - styled.components
  * - Features: 
  * 
- *     --> writting 'StyledHeading2Optimized' styles 
+ *     --> writting 'StyledSpaceY4Optimized' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,14 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const StyledSpaceY4Optimized = styled.div`
+  margin-top: 1rem;
+
+  > * + * {
+	margin-top: 1rem;
+  }
+`;
 
 export const StyledHeading2Optimized = styled.h2`
 	margin-bottom: 1rem;
