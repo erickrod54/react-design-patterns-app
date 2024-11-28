1eeb5767eada9fc8465e28f33d60705741a4be9f
Erick Rodriguez <erickrod54@gmail.com>
2024-11-27 21:31:00 -0500

    react-design-patterns-app - version 48.05 - styled.components - Features: writting trottling-rendering-optimization styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 46.16 - styled.components
+/**react-design-patterns-app - version 48.05 - styled.components
  * - Features: 
  * 
- *     --> writting 'rendering-optimization-state-allocatio' styles 
+ *     --> writting 'trottling-rendering-optimization' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -1331,6 +1331,12 @@ export const CustomTitleStyle = styled.div`
 .state-management-patterns{
 	span{
 			${tw`text-indigo-400 underline`}
+		}	
+}
+
+.trottling-rendering-optimization{
+	span{
+		${tw`text-red-500 underline`}
 		}	
 }
 
