e211497f0295a3e22c6938dfe150d1eebb3c2e43
Erick Rodriguez <erickrod54@gmail.com>
2024-11-28 21:30:00 -0500

    react-design-patterns-app - version 48.14 - TrackMouse - Features: writting TrackMouseWrapper styled component

--- src/components/optimization.patterns.track.mouse.jsx
+++ src/components/optimization.patterns.track.mouse.jsx
@@ -1,21 +1,25 @@
 import { useMousePosition } from "../hooks/optimization.patterns.use.mouse.hook";
 
-/**react-design-patterns-app - version 48.08 - TrackMouse
+/**react-design-patterns-app - version 48.14 - TrackMouse
  * - Features: 
  * 
- *     --> Building  'TrackMouse' 
+ *     --> writting  'TrackMouseWrapper' styled component
  *
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
 */
 
+const TrackMouseWrapper = styled.div`
+
+`
+
 const TrackMouse = (props) => {
   const position = useMousePosition();
 
   return (
-    <div>
-      Last mouse position - x: {position.x}, y: {position.y}
-    </div>
+    <TrackMouseWrapper>
+        Last mouse position - <span> x: {position.x}, y: {position.y} </span>
+    </TrackMouseWrapper>
   );
 };
 
