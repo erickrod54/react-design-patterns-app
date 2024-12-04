e717907e129b49033dc34ca62cb3a922de0402bd
Erick Rodriguez <erickrod54@gmail.com>
2024-12-03 21:35:00 -0500

    react-design-patterns-app - version 49.15 - Search - Features: writting SearchContainer styles

--- src/components/optimization.patterns.search.component.debouncing.jsx
+++ src/components/optimization.patterns.search.component.debouncing.jsx
@@ -1,15 +1,26 @@
+import styled from "styled-components";
 import { searchMeals } from "../api/api.layer.index";
 import React, { useMemo, useState } from "react";
 
-/**react-design-patterns-app - version 49.09 - Search
+/**react-design-patterns-app - version 49.15 - Search
  * - Features: 
  * 
- *     --> Building  'Search ' 
+ *     --> writting  'SearchContainer '  styles
  *
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
 */
 
+  const SearchContainer = styled.div`
+    width: 50%;
+    margin: 50px auto;
+    padding: 20px;
+    background-color: #222; /* Dark gray for contrast */
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(255, 255, 255, 0.2);
+    text-align: center;
+  `
+  
 const Search = () => {
   const [query, setQuery] = useState("");
   const [meals, setMeals] = useState([]);
@@ -27,7 +38,7 @@ const Search = () => {
   };
 
   return (
-    <div>
+    <SearchContainer>
       <form>
         <label>Search meals</label>
         <input type="text" value={query} onChange={onChangeQuery} />
@@ -37,7 +48,7 @@ const Search = () => {
           return <li key={meal.idMeal}>{meal.strMeal}</li>;
         })}
       </ul>
-    </div>
+    </SearchContainer>
   );
 };
 
