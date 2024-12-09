2cd4626b737f041ed8ac1a757c92689d985b682d
Erick Rodriguez <erickrod54@gmail.com>
2024-12-08 21:33:00 -0500

    react-design-patterns-app - version 50.11 - styled.components - Features: writting SearchForm styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 50.10 - styled.components
+/**react-design-patterns-app - version 50.11 - styled.components
  * - Features: 
  * 
- *     --> writting 'SearchContainer' styles 
+ *     --> writting 'SearchForm' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const SearchForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 10px;
+`
 
 export const SearchContainer = styled.div`
 	width: 50%;
