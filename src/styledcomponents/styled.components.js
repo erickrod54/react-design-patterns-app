108317aa82f4feeed9c7ef9b6b380bead7146ec0
Erick Rodriguez <erickrod54@gmail.com>
2024-12-09 21:32:00 -0500

    react-design-patterns-app - version 50.13 - styled.components - Features: Changing name SearchLable to SearchLabelNoDebounce

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 50.12 - styled.components
+/**react-design-patterns-app - version 50.13 - styled.components
  * - Features: 
  * 
- *     --> writting 'SearchLabel' styles 
+ *     --> Changing name 'SearchLabel'  to 'SearchLabelNoDebounce' styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -17,7 +17,7 @@ import tw from 'twin.macro';
  * to use them in the custom titles
  */
 
-export const SearchLabel = styled.label`
+export const SearchLabelNoDebounce = styled.label`
 	font-size: 18px;
 	font-weight: bold;
 	color: #ffcc00; /* Bright yellow for contrast */
