b56bd2089dc392ba89000c0a3aea2c0b31091bc4
Erick Rodriguez <erickrod54@gmail.com>
2024-12-10 21:31:00 -0500

    react-design-patterns-app - version 50.14 - Search - Features: Migrating SearchInput

--- src/components/optimization.patterns.search.component.debouncing.jsx
+++ src/components/optimization.patterns.search.component.debouncing.jsx
@@ -1,32 +1,17 @@
 import styled from "styled-components";
 import { searchMeals } from "../api/api.layer.index";
 import React, { useMemo, useState } from "react";
-import { SearchContainer, SearchForm, SearchLabelNoDebounce } from "../styledcomponents/styled.components";
+import { SearchContainer, SearchForm, SearchInput, SearchLabelNoDebounce } from "../styledcomponents/styled.components";
 
-/**react-design-patterns-app - version 50.13 - Search
+/**react-design-patterns-app - version 50.14 - Search
  * - Features: 
  * 
- *     --> Migrating  'SearchLabelNoDebounce ' 
+ *     --> Migrating  'SearchInput ' 
  *
  * Note: 'UserInfo' with a few modifications ends up in 
  * 'UserInfoWithHook'  
 */
 
- 
-const SearchInput = styled.input`
-  width: 80%;
-  padding: 10px;
-  font-size: 16px;
-  border: 2px solid #ffcc00; /* Bright yellow border */
-  border-radius: 5px;
-  background-color: #333; /* Dark gray */
-  color: white;
-  outline: none;
-
-  .search-input::placeholder {
-  color: #bbb;
-  }
-`
 
 const SearchResults = styled.ul`
   list-style: none;
