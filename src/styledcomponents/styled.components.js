f2953581c7dc5cb254cdeb4671d65135d318344c
Erick Rodriguez <erickrod54@gmail.com>
2024-12-10 21:35:00 -0500

    react-design-patterns-app - version 50.16 - styled.components - Features: writting SearchItem styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 50.15 - styled.components
+/**react-design-patterns-app - version 50.16 - styled.components
  * - Features: 
  * 
- *     --> writting 'SearchResults'  styles 
+ *     --> writting 'SearchItem'  styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,15 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const SearchItem = styled.li`
+	padding: 10px;
+	margin: 5px 0;
+	background-color: #444; /* Slightly lighter gray */
+	border-radius: 5px;
+	transition: background 0.3s;
+	cursor: pointer;
+`
 
 export const SearchResults = styled.ul`
 	list-style: none;
