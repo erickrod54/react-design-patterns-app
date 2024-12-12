1347eda55649d0b5a364c76edd0351d7ab13f1f8
Erick Rodriguez <erickrod54@gmail.com>
2024-12-11 21:35:00 -0500

    react-design-patterns-app - version 50.18 - styled.components - Features: writting SearchFormDebounce styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 50.17 - styled.components
+/**react-design-patterns-app - version 50.18 - styled.components
  * - Features: 
  * 
- *     --> writting 'SearchContainerDebouncing'  styles 
+ *     --> writting 'SearchFormDebounce'  styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,13 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const SearchFormDebounce = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 10px;
+`
 
 export const SearchContainerDebouncing = styled.div`
 	width: 50%;
