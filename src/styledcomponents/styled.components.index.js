dc0280d8ceffc3ac5dfbacfcd3a3e1332b7300e3
Erick Rodriguez <erickrod54@gmail.com>
2024-12-11 21:33:00 -0500

    react-design-patterns-app - version 50.17 - styledcomponents > index - Features: Importing and exporting SearchContainerDebouncing

--- src/styledcomponents/styled.components.index.js
+++ src/styledcomponents/styled.components.index.js
@@ -98,6 +98,7 @@ import { AlertWrapper,
          QuotesContainerScroll,
          ReviewsContainer,
          SearchContainer,
+         SearchContainerDebouncing,
          SearchForm,
          SearchInput,
          SearchItem,
@@ -159,10 +160,10 @@ import { AlertWrapper,
          ToolTipWrapper, UpdateTaskHeaderBoard, UpdateTaskHeaderImmer, UpdateTaskInputBoard, UpdateTaskInputImmer, WriterContainer } from "./styled.components";
         
          
-/**react-design-patterns-app - version 50.16 - styledcomponents > index
+/**react-design-patterns-app - version 50.17 - styledcomponents > index
  * - Features: 
  * 
- *     --> Importing and exporting 'SearchItem' 
+ *     --> Importing and exporting 'SearchContainerDebouncing' 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -331,4 +332,5 @@ export { SectionWrapper,
          SearchLabelNoDebounce,
          SearchInput,
          SearchResults,
+         SearchContainerDebouncing,
          QuoteTextPagination, ButtonPostForm, FormGroup, MealListContainer, MealItem }
