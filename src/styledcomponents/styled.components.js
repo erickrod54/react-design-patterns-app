5744c0a561cb6f3f1b4fff505822abcf5362e79d
Erick Rodriguez <erickrod54@gmail.com>
2024-12-12 21:32:00 -0500

    react-design-patterns-app - version 50.19 - styled.components - Features: writting SearchLabelDebounce styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 50.18 - styled.components
+/**react-design-patterns-app - version 50.19 - styled.components
  * - Features: 
  * 
- *     --> writting 'SearchFormDebounce'  styles 
+ *     --> writting 'SearchLabelDebounce'  styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -16,6 +16,12 @@ import tw from 'twin.macro';
  * i have to create a root access custom color palette in order 
  * to use them in the custom titles
  */
+
+export const SearchLabelDebounce = styled.label`
+	font-size: 18px;
+	font-weight: bold;
+	color: #ffcc00; /* Bright yellow for contrast */
+`
 
 export const SearchFormDebounce = styled.form`
 	display: flex;
