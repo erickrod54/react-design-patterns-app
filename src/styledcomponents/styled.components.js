e3b7de9e088fe5505258716952878bdb9e63a374
Erick Rodriguez <erickrod54@gmail.com>
2024-12-16 21:33:00 -0500

    react-design-patterns-app - version 51.14 - styled.components - Features: Refactoring StyledSpaceY4Optimized styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 51.13 - styled.components
+/**react-design-patterns-app - version 51.14 - styled.components
  * - Features: 
  * 
- *     --> Refactoring 'SearchContainer'  styles 
+ *     --> Refactoring 'StyledSpaceY4Optimized'  styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -66,12 +66,8 @@ export const SearchContainer = styled.div`
 `
 
 export const StyledSpaceY4Optimized = styled.div`
-  margin-top: 1rem;
-
-  > * + * {
-	margin-top: 1rem;
-  }
-`;
+	${tw`mt-4 space-y-4`}	
+`
 
 export const StyledHeading2Optimized = styled.h2`
 	margin-bottom: 1rem;
