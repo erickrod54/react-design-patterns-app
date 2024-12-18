099dd9bda907b1e3ae5e3fecd12fdb4a5e869768
Erick Rodriguez <erickrod54@gmail.com>
2024-12-17 21:30:00 -0500

    react-design-patterns-app - version 51.17 - styled.components - Features: Refactoring StyledListItemOptimized styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 51.16 - styled.components
+/**react-design-patterns-app - version 51.17 - styled.components
  * - Features: 
  * 
- *     --> Refactoring 'StyledButtonOptimized'  styles 
+ *     --> Refactoring 'StyledListItemOptimized'  styles 
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -101,11 +101,7 @@ export const StyledButtonOptimized = styled.button`
 `;
 
 export const StyledListItemOptimized = styled.li`
-	padding-top: 0.75rem; /* Adjust as needed */
-	padding-bottom: 0.75rem; /* Adjust as needed */
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+	${tw`pt-3 pb-3 flex justify-between items-center`}	
 `;
 
 export const StyledListOptimized = styled.ul`
