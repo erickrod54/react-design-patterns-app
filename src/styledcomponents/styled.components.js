8916aa198be7c581b523d76ba0c64105579859e8
Erick Rodriguez <erickrod54@gmail.com>
2024-12-18 21:30:00 -0500

    react-design-patterns-app - version 52.02 - styled.components - Features: Refactoring StyledListIngredient styles

--- src/styledcomponents/styled.components.js
+++ src/styledcomponents/styled.components.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 /**this is the tw macro import */
 import tw from 'twin.macro';
 
-/**react-design-patterns-app - version 52.01 - styled.components
+/**react-design-patterns-app - version 52.02 - styled.components
  * - Features: 
  * 
- *     --> Refactoring 'StyledListItemIngredient'  styles more  
+ *     --> Refactoring 'StyledListIngredient'  styles   
  * 
  * Note: the macro 'tw' in order to work and use has to be installed
  * there is around 4 or 5 libraries and this is oen of the most active
@@ -121,10 +121,7 @@ export const StyledListItemIngredient = styled.li`
 `;
 
 export const StyledListIngredient = styled.ul`
-	border-color: #d1d5db; /* Replace with your desired color */
-	border-width: 0;
-	border-style: solid;
-	border-top-width: 1px; /* Add this line for the first item */
+	${tw`border-t border-solid border-gray-300`}		
 `;
 
 export const StyledContainerIngredient = styled.div`
