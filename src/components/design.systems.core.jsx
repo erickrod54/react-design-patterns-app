f80bb7faa75cd882aa78b9f6b0fbf47844a485a2
Erick Rodriguez <erickrod54@gmail.com>
2024-12-18 21:35:00 -0500

    react-design-patterns-app - version 52.04 - FetchingDataWithReactQuery - Features: Adding DesignSystemsCore pillars

--- src/components/design.systems.core.jsx
+++ src/components/design.systems.core.jsx
@@ -4,10 +4,10 @@ import CustomTitle from "./custom.title.component";
 import { usePatternsAppContext } from "../context";
 
 
-/**react-design-patterns-app - version 52.03 - FetchingDataWitReactQuery
+/**react-design-patterns-app - version 52.04 - FetchingDataWitReactQuery
  * - Features: 
  * 
- *     --> Starting 'DesignSystemCore' 
+ *     --> Adding 'DesignSystemCore' pillars
  * 
  * Note: This is the introduction to design systems
 */
@@ -27,8 +27,24 @@ const DesignSystemCore = () => {
                 
                 <p>
                     the design and development of robust systems, along wiht cohesive design systems is by understanding and 
-                    <span className="text-white font-semibold"> integrating all these 4 pillars </span>
+                    <span className="text-white font-semibold"> integrating all these 4 pillars </span> and these are 
+                    as follows:
                 </p>
+
+            <ul className="list-white-space">
+                <li>
+                    <span className="text-white font-semibold"> Design Language </span> (  The foundation of a design system, including color palettes and typography )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> UI Kits </span> ( Tools like Figma to create and organize design components )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> Component Libraries </span> ( Using libraries like React and Angular to build reusable UI components )
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> Style Guides </span> ( ools like Gatsby, JS, and Storybook to document and implement the visual and functional aspects of a design system. )
+                </li>
+            </ul>
             </SectionWrapper>
         </div>
     )
