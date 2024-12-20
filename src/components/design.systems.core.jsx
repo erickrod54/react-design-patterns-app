baefe0417bb2eb583e70b18d3e3929060da1ebcc
Erick Rodriguez <erickrod54@gmail.com>
2024-12-19 21:31:00 -0500

    react-design-patterns-app - version 52.05 - FetchingDataWithReactQuery - Features: Adding Design Language concepts

--- src/components/design.systems.core.jsx
+++ src/components/design.systems.core.jsx
@@ -4,10 +4,10 @@ import CustomTitle from "./custom.title.component";
 import { usePatternsAppContext } from "../context";
 
 
-/**react-design-patterns-app - version 52.04 - FetchingDataWitReactQuery
+/**react-design-patterns-app - version 52.05 - FetchingDataWitReactQuery
  * - Features: 
  * 
- *     --> Adding 'DesignSystemCore' pillars
+ *     --> Adding ' Design Language' concepts
  * 
  * Note: This is the introduction to design systems
 */
@@ -45,6 +45,27 @@ const DesignSystemCore = () => {
                     <span className="text-white font-semibold"> Style Guides </span> ( ools like Gatsby, JS, and Storybook to document and implement the visual and functional aspects of a design system. )
                 </li>
             </ul>
+
+                <p>
+                    so taking some more detail on <span className="text-white font-semibold"> every design system pillar </span> 
+                    , starting by <span className="text-white font-semibold"> Design Language </span>:
+                </p>
+
+            <ul className="list-white-space">
+                <li>
+                    This is the foundation of a design system and includes <span className="text-white font-semibold"> color palettes, typography, spacing, grid systems, and more. It defines 
+                    the visual identity of a product or brand. </span> 
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> Color palettes </span> help maintain <span className="text-white font-semibold"> consistent color usage across a product,
+                    </span> ensuring <span className="text-white font-semibold"> accessibility and brand recognition. </span>
+                </li>
+                <li>
+                    <span className="text-white font-semibold"> Typography </span> determines the <span className="text-white font-semibold"> fonts, sizes, and line spacings </span>  that 
+                    create a <span className="text-white font-semibold"> readable and cohesive experience across different devices. </span>
+                </li>
+            </ul>
+                
             </SectionWrapper>
         </div>
     )
